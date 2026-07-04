9e4e50a1ad8c73252126e103a02ea41550da804b
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 00:06:43 +0330

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,11 @@
 import uvicorn
 
+from app.db.database import engine
+from app.db.models import Base
+
+# Create database tables
+Base.metadata.create_all(bind=engine)
+
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
