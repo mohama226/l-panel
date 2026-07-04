4803088f1bb1ee933a29d576fea154a21fbf15e2
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 02:16:32 +0330

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -7,8 +7,10 @@ pwd_context = CryptContext(
 
 
 def hash_password(password: str):
+
     return pwd_context.hash(password)
 
 
-def verify_password(password, hashed):
+def verify_password(password: str, hashed: str):
+
     return pwd_context.verify(password, hashed)
