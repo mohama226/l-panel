3d4d7c622450e8cef47997d713816e12f95a42dd
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 02:04:05 +0330

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,11 @@
 from pathlib import Path
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from app.core.config import settings
-from app.db.database import Base, engine
-
-from app.routers.auth import router as auth_router
-from app.routers.dashboard import router as dashboard_router
-
-
-# ساخت جداول دیتابیس
-Base.metadata.create_all(bind=engine)
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -22,21 +14,17 @@ app = FastAPI(
     version=settings.APP_VERSION
 )
 
-# فایل‌های استاتیک
+# Static Files
 app.mount(
     "/static",
     StaticFiles(directory=str(BASE_DIR / "static")),
     name="static"
 )
 
-# قالب‌ها
+# Templates
 templates = Jinja2Templates(
     directory=str(BASE_DIR / "templates")
 )
-
-# ثبت Router ها
-app.include_router(auth_router)
-app.include_router(dashboard_router)
 
 
 @app.get("/")
