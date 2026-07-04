fdde76b3c007982756c5de8c5f233c8b59fbace8
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-04 19:54:52 +0330

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,10 +5,18 @@ app = FastAPI(
     version="0.0.1"
 )
 
+
 @app.get("/")
-async def root():
+async def home():
     return {
         "project": "LAK Panel",
         "version": "0.0.1",
         "status": "running"
+    }
+
+
+@app.get("/health")
+async def health():
+    return {
+        "status": "ok"
     }
