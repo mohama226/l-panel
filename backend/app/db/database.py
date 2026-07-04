934c9b35a54e5294e552ddb3e7316d5dee593dfd
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 00:30:22 +0330

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,5 @@
 from sqlalchemy import create_engine
-
 from sqlalchemy.orm import declarative_base
-
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./database.db"
@@ -20,3 +18,16 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+
+        yield db
+
+    finally:
+
+        db.close()
