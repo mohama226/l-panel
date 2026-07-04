ed0a6842df68aaf458e5c22f274cc29200327f3e
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-04 21:06:48 +0330

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,37 @@
 from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(
     title="LAK Panel",
-    version="0.0.1"
+    version="0.0.2"
 )
 
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
 
-@app.get("/")
-async def home():
-    return {
-        "project": "LAK Panel",
-        "version": "0.0.1",
-        "status": "running"
-    }
+templates = Jinja2Templates(
+    directory="app/templates"
+)
 
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request
+        }
+    )
 
 @app.get("/health")
 async def health():
+
     return {
         "status": "ok"
     }
