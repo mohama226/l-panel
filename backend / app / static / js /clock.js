5f1537e0dd8801f7dca7a7996894334a7b658a5f
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 16:35:13 +0330

    Update clock.js

--- backend / app / static / js /clock.js
+++ backend / app / static / js /clock.js
@@ -1,27 +1,6 @@
-function updateClock() {
-
+document.addEventListener("DOMContentLoaded", function () {
     const el = document.getElementById("datetime");
-
-    if (!el) return;
-
-    const now = new Date();
-
-    const date = now.toLocaleDateString("en-GB", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric"
-    });
-
-    const time = now.toLocaleTimeString("en-GB", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false
-    });
-
-    el.innerHTML = "📅 " + date + " &nbsp;&nbsp; 🕒 " + time;
-}
-
-updateClock();
-setInterval(updateClock, 1000);
+    if (el) {
+        el.innerHTML = "CLOCK OK";
+    }
+});
