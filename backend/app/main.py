0a7450a26849d98b74795fd959fab6929066dea4
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-06 10:00:29 +0330

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from app.routers.auth import router as auth_router
 from app.routers.dashboard import router as dashboard_router
 from app.routers.settings import router as settings_router
+from app.routers.users import router as users_router
 
 app = FastAPI(
     title="LAK PANEL",
@@ -18,3 +19,4 @@ app.mount(
 app.include_router(auth_router)
 app.include_router(dashboard_router)
 app.include_router(settings_router)
+app.include_router(users_router)
