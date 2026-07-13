f1932a6d575d5fc7b0757bc4e0003a61dbe3e9a1
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 17:42:45 +0330

    Create functions.sh

---
+++ install/functions.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+green() {
+
+    echo -e "\e[32m$1\e[0m"
+
+}
+
+red() {
+
+    echo -e "\e[31m$1\e[0m"
+
+}
+
+yellow() {
+
+    echo -e "\e[33m$1\e[0m"
+
+}
+
+blue() {
+
+    echo -e "\e[36m$1\e[0m"
+
+}
+
+error_exit() {
+
+    red "$1"
+
+    exit 1
+
+}
+
+check_root() {
+
+    if [[ $EUID -ne 0 ]]; then
+
+        error_exit "Please run installer as root."
+
+    fi
+
+}
+
+check_os() {
+
+    source /etc/os-release
+
+    if [[ "$ID" != "ubuntu" ]]; then
+
+        error_exit "Only Ubuntu is supported."
+
+    fi
+
+}
+
+create_directories() {
+
+    mkdir -p "$INSTALL_DIR"
+
+    mkdir -p "$CONFIG_DIR"
+
+    mkdir -p "$LOG_DIR"
+
+    mkdir -p "$BACKUP_DIR"
+
+    mkdir -p "$TMP_DIR"
+
+    mkdir -p "$UPLOAD_DIR"
+
+}
