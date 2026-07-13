1889868761d740babdefe817bf0569439605f396
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 20:00:45 +0330

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+from app.database import Base, engine
+
+from app.admin import router as admin_router
+
+
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI(
+    title="L-Panel",
+    version="1.0.0"
+)
+
+
+app.include_router(
+    admin_router
+)
+
+
+@app.get("/")
+def home():
+
+    return {
+        "name": "L-Panel",
+        "status": "running"
+    }
