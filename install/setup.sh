7c57124447cf1bb088f24aee744b88cace4cb7a8
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 19:45:25 +0330

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-source "$(dirname "$0")/variables.sh"
-source "$(dirname "$0")/functions.sh"
-source "$(dirname "$0")/logo.sh"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$SCRIPT_DIR/variables.sh"
+
+source "$SCRIPT_DIR/functions.sh"
+
+source "$SCRIPT_DIR/logo.sh"
 
 check_root
-check_os
+
 wait_for_apt
+
 create_directories
-install_packages
 
-bash "$(dirname "$0")/postgres.sh"
-bash "$(dirname "$0")/env.sh"
-bash "$(dirname "$0")/python.sh"
+bash "$SCRIPT_DIR/postgres.sh"
 
-bash "$(dirname "$0")/nginx.sh"
-bash "$(dirname "$0")/service.sh"
-bash "$(dirname "$0")/ssl.sh"
-bash "$(dirname "$0")/firewall.sh"
-bash "$(dirname "$0")/ocserv.sh"
+bash "$SCRIPT_DIR/python.sh"
 
-# تنظیم مجوزها و ایجاد symlink برای دستور l-panel
-ln -sf /opt/l-panel/cli/l-panel /usr/local/bin/l-panel
-chmod +x /usr/local/bin/l-panel
+bash "$SCRIPT_DIR/service.sh"
 
-green "Installation Finished."
+bash "$SCRIPT_DIR/nginx.sh"
+
+bash "$SCRIPT_DIR/firewall.sh"
+
+bash "$SCRIPT_DIR/ssl.sh"
+
+bash "$SCRIPT_DIR/ocserv.sh"
+
+green "Installation Completed."
