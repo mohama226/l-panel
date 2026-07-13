a281cab21cfd81f66acd28fadf4202f92760c229
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 19:51:51 +0330

    Update nginx.sh

--- install/nginx.sh
+++ install/nginx.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+source "$(dirname "$0")/variables.sh"
+
+source "$(dirname "$0")/functions.sh"
+
 
 blue "Installing Nginx..."
 
@@ -11,29 +15,35 @@ cat > /etc/nginx/sites-available/l-panel <<EOF
 
 server {
 
-listen 80;
+    listen 80;
+
+    server_name _;
 
 
-server_name _;
+    location / {
 
+        proxy_pass http://127.0.0.1:${PANEL_PORT};
 
-location / {
+        proxy_http_version 1.1;
 
-proxy_pass http://127.0.0.1:${PANEL_PORT};
+        proxy_set_header Host \$host;
 
+        proxy_set_header X-Real-IP \$remote_addr;
 
-proxy_set_header Host \$host;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 
-proxy_set_header X-Real-IP \$remote_addr;
-
-}
+    }
 
 }
 
 EOF
 
 
-ln -sf /etc/nginx/sites-available/l-panel /etc/nginx/sites-enabled/l-panel
+rm -f /etc/nginx/sites-enabled/default
+
+
+ln -sf /etc/nginx/sites-available/l-panel \
+/etc/nginx/sites-enabled/l-panel
 
 
 nginx -t
@@ -42,4 +52,4 @@ nginx -t
 systemctl restart nginx
 
 
-green "Nginx Ready."
+green "Nginx configured."
