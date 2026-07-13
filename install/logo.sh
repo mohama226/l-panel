fcd2eeab6c7cacd8623470b885be7817165975b0
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 17:43:19 +0330

    Create logo.sh

---
+++ install/logo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+
+echo "========================================"
+
+echo "            L-PANEL INSTALLER"
+
+echo "========================================"
+
+echo ""
+
+echo "Version : $APP_VERSION"
+
+echo ""
