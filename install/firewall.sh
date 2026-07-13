0ec992cea411baace94711ff818dd55f4cb53a60
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 19:52:12 +0330

    Update firewall.sh

--- install/firewall.sh
+++ install/firewall.sh
@@ -1,3 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-blue "Firewall configuration skipped."
+
+source "$(dirname "$0")/variables.sh"
+
+
+if command -v ufw >/dev/null 2>&1
+
+then
+
+ufw allow 22/tcp
+
+ufw allow 80/tcp
+
+ufw allow ${PANEL_PORT}/tcp
+
+ufw --force enable
+
+
+fi
+
+
+echo "Firewall configured."
