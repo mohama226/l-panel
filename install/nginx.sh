f166d160c5ecc63a67a9c9aef0cc13cccbc35dfb
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 18:42:40 +0330

    Update nginx.sh

--- install/nginx.sh
+++ install/nginx.sh
@@ -1,7 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-blue "Installing Nginx ..."
+
+blue "Installing Nginx..."
+
 
 apt install -y nginx
 
-green "Nginx Installed."
+
+cat > /etc/nginx/sites-available/l-panel <<EOF
+
+server {
+
+listen 80;
+
+
+server_name _;
+
+
+location / {
+
+proxy_pass http://127.0.0.1:${PANEL_PORT};
+
+
+proxy_set_header Host \$host;
+
+proxy_set_header X-Real-IP \$remote_addr;
+
+}
+
+}
+
+EOF
+
+
+ln -sf /etc/nginx/sites-available/l-panel /etc/nginx/sites-enabled/l-panel
+
+
+nginx -t
+
+
+systemctl restart nginx
+
+
+green "Nginx Ready."
