fd1411dc66b8dc29fa5d24aa898cd5132ac6a5cd
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 19:48:46 +0330

    Update python.sh

--- install/python.sh
+++ install/python.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
 
-blue "Creating Python Virtual Environment..."
+
+source "$(dirname "$0")/variables.sh"
+
+source "$(dirname "$0")/functions.sh"
+
+
+
+blue "Installing Python Environment"
+
+
 
 cd "$INSTALL_DIR/backend"
 
+
+
 python3 -m venv venv
+
+
 
 source venv/bin/activate
 
+
+
 pip install --upgrade pip
+
+
+
+if [ -f requirements.txt ]
+
+then
 
 pip install -r requirements.txt
 
-green "Python Installed Successfully."
+fi
+
+
+
+green "Python ready."
