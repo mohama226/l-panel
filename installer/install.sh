09d064e65133262a1b061ab6d7655e527f8742d2
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-14 21:44:00 +0330

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -2,20 +2,33 @@
 set -e
 
 APP_DIR="/opt/l-panel"
+ZIP_URL="https://github.com/mohama226/l-panel/archive/refs/heads/main.zip"
+
+echo "Installing L-Panel..."
 
 apt update
-apt install -y git python3 python3-pip python3-venv
+apt install -y curl unzip python3 python3-pip python3-venv
 
 rm -rf "$APP_DIR"
+mkdir -p /opt
 
-git clone https://github.com/mohama226/l-panel.git "$APP_DIR"
+cd /tmp
+rm -f l-panel.zip
+
+curl -L "$ZIP_URL" -o l-panel.zip
+
+unzip -oq l-panel.zip
+
+mv l-panel-main "$APP_DIR"
 
 cd "$APP_DIR"
 
 python3 -m venv venv
+
 source venv/bin/activate
 
 pip install --upgrade pip
+
 pip install -r requirements.txt
 
 python3 run.py
