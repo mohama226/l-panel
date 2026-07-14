8dc474e462cd55af04074a095d0b40a446463546
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-14 22:47:00 +0330

    Create system_info.sh

---
+++ installer/system_info.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear
+
+echo "=========== SYSTEM INFO ==========="
+
+echo
+
+hostnamectl
+
+echo
+
+free -h
+
+echo
+
+df -h
+
+echo
+
+uptime
+
+echo
+
+ip addr
+
+echo
+
+read -p "Press Enter..."
