c92887b53df7436bd895d9216d3fae30fef466a9
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 00:37:15 +0330

    Update install_ocserv.sh

--- installer/install_ocserv.sh
+++ installer/install_ocserv.sh
@@ -1,51 +1,56 @@
 #!/bin/bash
 
-source /opt/l-panel/installer/core.sh
+set -e
 
 
-echo "Installing Ocserv 1.5.0"
+echo "=============================="
+echo " Installing OCServ 1.5.0"
+echo "=============================="
+
+
+BASE="/opt/l-panel"
+OCVER="1.5.0"
 
 
 apt update
 
-
 apt install -y \
 git \
+wget \
 build-essential \
 pkg-config \
-gnutls-bin \
 libgnutls28-dev \
-libnl-route-3-dev \
-libnl-3-dev \
-libseccomp-dev \
 libreadline-dev \
-libpam0g-dev \
-libwrap0-dev \
+libnl-route-3-dev \
+libseccomp-dev \
 liblz4-dev \
-libev-dev \
+libprotobuf-c-dev \
+protobuf-c-compiler \
+libpcl1-dev \
+libhttp-parser-dev \
 autoconf \
 automake \
-libtool \
-gettext \
-wget
+libtool
+
+
+mkdir -p /usr/local/src
 
 
 cd /usr/local/src
 
 
-rm -rf ocserv
+if [ ! -f ocserv-${OCVER}.tar.xz ]; then
+
+wget \
+https://github.com/openconnect/ocserv/releases/download/${OCVER}/ocserv-${OCVER}.tar.xz
+
+fi
 
 
-git clone https://github.com/openconnect/ocserv.git
+tar xf ocserv-${OCVER}.tar.xz
 
 
-cd ocserv
-
-
-git checkout v1.5.0
-
-
-./autogen.sh
+cd ocserv-${OCVER}
 
 
 ./configure \
@@ -55,7 +60,6 @@ git checkout v1.5.0
 
 make -j$(nproc)
 
-
 make install
 
 
@@ -63,76 +67,42 @@ make install
 mkdir -p /etc/ocserv
 
 
-cp /opt/l-panel/installer/configs/ocserv.conf \
+cp $BASE/installer/files/ocserv/ocserv.conf \
 /etc/ocserv/ocserv.conf
 
 
 
-if [ ! -f /etc/ocserv/server-cert.pem ]
-then
-
-echo "Generating certificate"
-
-
-certtool --generate-privkey \
---outfile /etc/ocserv/server-key.pem
-
-
-cat >/tmp/ocserv-cert.cfg <<EOF
-organization = L-PANEL
-cn = VPN Server
-expiration_days = 3650
-EOF
-
-
-certtool \
---generate-certificate \
---load-privkey /etc/ocserv/server-key.pem \
---outfile /etc/ocserv/server-cert.pem \
---template /tmp/ocserv-cert.cfg
-
-fi
+cp $BASE/installer/files/ocserv/ocserv.conf \
+/etc/ocserv/ocserv.conf.backup
 
 
 
-echo "Creating password file"
+echo "Creating SSL certificate"
+
+
+openssl req \
+-x509 \
+-newkey rsa:4096 \
+-keyout /etc/ocserv/server-key.pem \
+-out /etc/ocserv/server-cert.pem \
+-days 3650 \
+-nodes \
+-subj "/CN=L-PANEL VPN"
+
 
 touch /etc/ocserv/ocpasswd
-
-
-echo "Creating systemd service"
-
-
-cat >/etc/systemd/system/ocserv.service <<EOF
-[Unit]
-Description=Ocserv VPN Server
-After=network.target
-
-
-[Service]
-ExecStart=/usr/sbin/ocserv -c /etc/ocserv/ocserv.conf
-Restart=always
-
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
 
 
 systemctl daemon-reload
 
 
-systemctl enable ocserv
+echo "OCServ installed"
 
 
-systemctl restart ocserv
+systemctl enable ocserv || true
 
 
+systemctl restart ocserv || true
 
-echo
-echo "================================"
-echo " Ocserv 1.5.0 Installed"
-echo "================================"
 
-systemctl status ocserv --no-pager
+echo "DONE"
