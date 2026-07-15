585c23e8b299b089cdba5c683b25301d691ba4a1
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 17:56:46 +0330

    Update servers.py

--- backend/routes/servers.py
+++ backend/routes/servers.py
@@ -1,1 +1,12 @@
-#
+from flask import Blueprint
+
+servers_bp = Blueprint(
+    "servers",
+    __name__,
+    url_prefix="/servers"
+)
+
+@servers_bp.route("/")
+def servers():
+
+    return "Servers"
