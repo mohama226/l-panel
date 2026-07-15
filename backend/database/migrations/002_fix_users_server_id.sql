b62f33625b29d5ffae074d1970d4b9726390217d
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 21:40:38 +0330

    Update 002_fix_users_server_id.sql

--- backend/database/migrations/002_fix_users_server_id.sql
+++ backend/database/migrations/002_fix_users_server_id.sql
@@ -1,17 +1,23 @@
 ALTER TABLE users
 ADD COLUMN IF NOT EXISTS server_id INTEGER;
 
+
 DO $$
 BEGIN
+
     IF NOT EXISTS (
         SELECT 1
         FROM information_schema.table_constraints
         WHERE constraint_name = 'users_server_id_fkey'
     )
+
     THEN
+
         ALTER TABLE users
         ADD CONSTRAINT users_server_id_fkey
         FOREIGN KEY (server_id)
         REFERENCES servers(id);
+
     END IF;
+
 END $$;
