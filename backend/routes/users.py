9642d64d1e84b2c3934a3944025a2466fba2ef94
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 23:09:07 +0330

    Update users.py

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,5 +1,7 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from backend.extensions import db
 from backend.models.user import User
+from datetime import datetime
 
 
 users_bp = Blueprint(
@@ -11,14 +13,151 @@ users_bp = Blueprint(
 
 
 @users_bp.route("/")
-def users():
+def index():
 
-    data = User.query.order_by(
-        User.username
+    users = User.query.order_by(
+        User.id.desc()
     ).all()
 
 
     return render_template(
         "users/index.html",
-        users=data
+        users=users
+    )
+
+
+
+# Create User
+
+@users_bp.route("/create", methods=["GET","POST"])
+def create():
+
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+
+        user = User(
+
+            username=username,
+            password=password,
+            status="active"
+
+        )
+
+
+        db.session.add(user)
+        db.session.commit()
+
+
+        return redirect(
+            url_for("users.index")
+        )
+
+
+
+    return render_template(
+        "users/create.html"
+    )
+
+
+
+
+# Edit User
+
+@users_bp.route("/edit/<int:id>", methods=["GET","POST"])
+def edit(id):
+
+    user = User.query.get_or_404(id)
+
+
+
+    if request.method=="POST":
+
+
+        user.username=request.form.get(
+            "username"
+        )
+
+        user.status=request.form.get(
+            "status"
+        )
+
+
+        expire=request.form.get(
+            "expire_date"
+        )
+
+
+        if expire:
+
+            user.expire_date=datetime.strptime(
+                expire,
+                "%Y-%m-%d"
+            )
+
+
+        db.session.commit()
+
+
+        return redirect(
+            url_for("users.index")
+        )
+
+
+    return render_template(
+        "users/edit.html",
+        user=user
+    )
+
+
+
+
+
+# Delete User
+
+@users_bp.route("/delete/<int:id>")
+def delete(id):
+
+    user=User.query.get_or_404(id)
+
+
+    db.session.delete(user)
+
+    db.session.commit()
+
+
+    return redirect(
+        url_for("users.index")
+    )
+
+
+
+
+
+# Block / Unblock
+
+@users_bp.route("/toggle/<int:id>")
+def toggle(id):
+
+    user=User.query.get_or_404(id)
+
+
+    if user.status=="active":
+
+        user.status="blocked"
+
+    else:
+
+        user.status="active"
+
+
+
+    db.session.commit()
+
+
+    return redirect(
+        url_for("users.index")
     )
