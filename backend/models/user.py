9d74e112e6cb9b68b8e2096b0fb65aa23c2972ec
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 08:13:40 +0330

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -6,12 +6,10 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-
     id = db.Column(
         db.Integer,
         primary_key=True
     )
-
 
     username = db.Column(
         db.String(64),
@@ -19,23 +17,19 @@ class User(db.Model):
         nullable=False
     )
 
-
     password = db.Column(
         db.String(255),
         nullable=False
     )
-
 
     status = db.Column(
         db.String(20),
         default="active"
     )
 
-
     expire_date = db.Column(
         db.DateTime
     )
-
 
     created_at = db.Column(
         db.DateTime,
