febf2972cb622dd6b3894caa0774d90a9ac3112f
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 22:33:22 +0330

    Update dashboard.py

--- backend/routes/dashboard.py
+++ backend/routes/dashboard.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template
 
+
 dashboard_bp = Blueprint(
     "dashboard",
     __name__
 )
 
-@dashboard_bp.route("/")
-def index():
 
-    return render_template("index.html")
+@dashboard_bp.route("/")
+def dashboard():
+
+    return render_template(
+        "dashboard.html"
+    )
