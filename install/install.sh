32a7a81a8a3253f38973f662f816094f87d51ed0
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-16 23:49:29 +0330

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -38,3 +38,12 @@ VALUES ('$ADMIN_USER', '$ADMIN_PASS');
 EOF
 
 date "+%Y-%m-%d %H:%M:%S" > last_update.txt
+
+echo "[+] Installing systemd service..."
+
+cp install/lpanel.service /etc/systemd/system/lpanel.service
+systemctl daemon-reload
+systemctl enable lpanel
+systemctl restart lpanel
+
+echo "[+] L-Panel service installed and running."
