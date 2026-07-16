717a45f3fd24e653f3b1648bb4f945cf2723fbdf
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-16 23:29:15 +0330

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,1 +1,11 @@
-#
+from .users import router as users_router
+from .dashboard import router as dashboard_router
+from .admin import router as admin_router
+from .ocserv import router as ocserv_router
+
+__all__ = [
+    "users_router",
+    "dashboard_router",
+    "admin_router",
+    "ocserv_router",
+]
