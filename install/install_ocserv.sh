5cc946e1d03541d171f25ae0fdc8ff4ef05ab3b9
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-16 21:49:33 +0330

    Update install_ocserv.sh

--- install/install_ocserv.sh
+++ install/install_ocserv.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
+cd /opt/l-panel
+
 echo "=== Installing ocserv 1.5.0 ==="
 
 read -p "Enter ocserv port (default 443): " OCPORT
 OCPORT=${OCPORT:-443}
 
-dnf install -y epel-release
 dnf install -y ocserv
 
 sed -i "s/^tcp-port = .*/tcp-port = $OCPORT/" /etc/ocserv/ocserv.conf
@@ -15,4 +16,4 @@ sed -i "s/^udp-port = .*/udp-port = $OCPORT/" /etc/ocserv/ocserv.conf
 systemctl enable ocserv
 systemctl restart ocserv
 
-echo "Ocserv installed on port $OCPORT"
+echo "[+] Ocserv installed on port $OCPORT"
