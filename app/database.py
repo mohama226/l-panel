20345aa4e59c3c53d237589b761a1aa5f797750d
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-16 21:53:12 +0330

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DB_URL = os.getenv("DB_URL", "postgresql://lpanel:1234@localhost:5432/lpanel")
+DB_URL = "postgresql://lpanel:1234@localhost:5432/lpanel"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
