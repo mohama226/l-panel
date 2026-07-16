34251b431dcdc6755c3e50a9ca7fca694fc0830e
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-16 21:12:15 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import users, system, ocserv
+from app.routers import users, admin, ocserv
 from app.database import init_db
 
 app = FastAPI(title="l-panel")
@@ -9,5 +9,5 @@ def startup():
     init_db()
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(system.router, prefix="/system", tags=["System"])
+app.include_router(admin.router, prefix="/admin", tags=["Admin"])
 app.include_router(ocserv.router, prefix="/ocserv", tags=["Ocserv"])
