1d976df5f2099d6e4751a87f9e24a6ed7c40c953
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-16 23:43:23 +0330

    Update deps.sh

--- install/deps.sh
+++ install/deps.sh
@@ -9,5 +9,8 @@ dnf install -y python3 python3-pip python3-devel
 dnf install -y gcc gcc-c++ make
 dnf install -y postgresql postgresql-server postgresql-devel
 dnf install -y ocserv
+dnf install -y python3-jinja2
+
+pip3 install Jinja2 uvicorn fastapi starlette
 
 echo "[+] All dependencies installed successfully."
