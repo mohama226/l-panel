a974182ea1231599e984e7ab0be483acef781d13
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 20:12:03 +0330

    Create header.php

---
+++ public/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>L-PANEL</title>
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
+
+<link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+
+<body>
+
+
+<div class="wrapper">
