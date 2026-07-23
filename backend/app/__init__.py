1955e7e1d99d18370b6bbe6952a452b91bcff901
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 11:31:26 +0330

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,26 +1,76 @@
 from flask import Flask
+
 from flask_cors import CORS
 
 from config import *
+
 from database import db
+
+
 
 def create_app():
 
     app = Flask(__name__)
 
+
     app.config["SECRET_KEY"] = SECRET_KEY
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        SQLALCHEMY_DATABASE_URI
+    )
+
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    CORS(app)
+
+
+    CORS(
+        app,
+        resources={
+            r"/api/*":{
+                "origins":"*"
+            }
+        }
+    )
+
+
 
     db.init_app(app)
 
-    from app.routes import api
 
-    app.register_blueprint(api,url_prefix="/api")
+
+    # Authentication Routes
+
+    from app.routes.auth import auth
+
+    app.register_blueprint(
+
+        auth,
+
+        url_prefix="/api"
+
+    )
+
+
+
+    @app.get("/api/health")
+    def health():
+
+        return {
+
+            "status":"ok",
+
+            "panel":"L-PANEL"
+
+        }
+
+
 
     with app.app_context():
+
         db.create_all()
+
+
 
     return app
