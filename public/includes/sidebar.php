f9345a7d3295356dafaaf27c2864f2353796f6a9
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 23:52:57 +0330

    Update sidebar.php

--- public/includes/sidebar.php
+++ public/includes/sidebar.php
@@ -1,45 +1,36 @@
 <div class="sidebar">
 
-
 <h2>
 L-PANEL
 </h2>
-
 
 <a href="/modiran">
 🏠 داشبورد
 </a>
 
-
-
 <a href="/modiran/users">
 👥 کاربران VPN
 </a>
-
-
 
 <a href="/modiran/users/create.php">
 ➕ افزودن کاربر
 </a>
 
-
-
 <a href="/modiran/users/bulk.php">
 👥 افزودن گروهی
 </a>
-
-
 
 <a href="#">
 ⚙ تنظیمات
 </a>
 
-
+<!-- 🔥 لینک جدید که خواستی -->
+<a href="/users">
+👤 ورود کاربران VPN
+</a>
 
 <a href="/logout.php">
 🚪 خروج
 </a>
-
-
 
 </div>
