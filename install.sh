49619594fc7edd7cbbe20fde30e606c3aea1ab01
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 18:24:32 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,58 +8,49 @@ echo "======================"
 echo " L-PANEL INSTALL"
 echo "======================"
 
-apt update
 
-apt install -y python3 python3-pip python3-venv git postgresql postgresql-contrib nginx
+if command -v apt >/dev/null 2>&1
+then
 
-if [ ! -d "$APP_DIR" ]; then
-    git clone https://github.com/mohama226/l-panel.git $APP_DIR
+    echo "Ubuntu/Debian detected"
+
+    apt update
+
+    apt install -y \
+    python3 \
+    python3-pip \
+    python3-venv \
+    git
+
+
+elif command -v dnf >/dev/null 2>&1
+then
+
+    echo "AlmaLinux/RHEL detected"
+
+    dnf install -y \
+    python3 \
+    python3-pip \
+    python3-virtualenv \
+    git
+
+
+elif command -v yum >/dev/null 2>&1
+then
+
+    echo "CentOS detected"
+
+    yum install -y \
+    python3 \
+    python3-pip \
+    python3-virtualenv \
+    git
+
+
+else
+
+    echo "Unsupported OS"
+
+    exit 1
+
 fi
-
-cd $APP_DIR
-
-python3 -m venv venv
-
-source venv/bin/activate
-
-pip install --upgrade pip
-
-pip install -r requirements.txt
-
-
-systemctl enable postgresql
-systemctl start postgresql
-
-
-sudo -u postgres psql <<EOF
-CREATE USER lpanel WITH PASSWORD 'lpanel123';
-CREATE DATABASE lpanel OWNER lpanel;
-EOF
-
-
-cat > /etc/systemd/system/lpanel.service <<EOF
-[Unit]
-Description=L-Panel
-After=network.target
-
-[Service]
-WorkingDirectory=/opt/l-panel
-ExecStart=/opt/l-panel/venv/bin/python /opt/l-panel/app.py
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-
-systemctl daemon-reload
-systemctl enable lpanel
-systemctl restart lpanel
-
-
-echo ""
-echo "======================"
-echo " INSTALL COMPLETE"
-echo " Panel:"
-echo " http://SERVER-IP:5000"
-echo "======================"
