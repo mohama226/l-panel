2ae1ab7966c58be0b4cf103caad84db156c659cf
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 00:03:14 +0330

    Update create.php

--- public/modiran/users/create.php
+++ public/modiran/users/create.php
@@ -4,184 +4,105 @@ require "../../../app/auth.php";
 checkLogin();
 
 require "../../../app/database.php";
+require "../../../app/ocserv.php";   // ← اضافه شد
 
-
-$msg="";
-
+$msg = "";
 
 if($_POST){
 
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    $expire   = $_POST['expire_date'];
+    $volume   = $_POST['total_gb'];
 
-$username=trim($_POST['username']);
+    if($username && $password){
 
-$password=trim($_POST['password']);
+        $hash = password_hash($password, PASSWORD_DEFAULT);
 
-$expire=$_POST['expire_date'];
+        $stmt = $db->prepare("
+            INSERT INTO users
+            (username,password,expire_date,total_gb)
+            VALUES
+            (?,?,?,?)
+        ");
 
-$volume=$_POST['total_gb'];
+        $stmt->execute([
+            $username,
+            $hash,
+            $expire,
+            $volume
+        ]);
 
+        // 🔥 اضافه شد: ساخت کاربر در ocserv
+        ocserv_user_add(
+            $username,
+            $password
+        );
 
-
-if($username && $password){
-
-
-$hash=password_hash(
-$password,
-PASSWORD_DEFAULT
-);
-
-
-
-$stmt=$db->prepare(
-"
-INSERT INTO users
-(username,password,expire_date,total_gb)
-VALUES
-(?,?,?,?)
-"
-);
-
-
-
-$stmt->execute([
-
-$username,
-$hash,
-$expire,
-$volume
-
-]);
-
-
-
-$msg="کاربر با موفقیت ساخته شد";
-
-
+        $msg = "کاربر با موفقیت ساخته شد";
+    }
 }
-
-
-}
-
-
 
 include "../../includes/header.php";
-
 include "../../includes/sidebar.php";
 
 ?>
 
-
-
 <div class="content">
-
 
 <div class="form-box">
 
-
-<h2>
-➕ افزودن کاربر VPN
-</h2>
-
-
+<h2>➕ افزودن کاربر VPN</h2>
 
 <?php if($msg): ?>
-
 <div class="alert-success">
-
-<?=$msg?>
-
+    <?=$msg?>
 </div>
-
 <?php endif; ?>
-
-
-
 
 <form method="post">
 
-
-
-<label>
-نام کاربری
-</label>
-
+<label>نام کاربری</label>
 <input 
-class="form-control"
-name="username"
-placeholder="مثلا user100"
-required>
+    class="form-control"
+    name="username"
+    placeholder="مثلا user100"
+    required
+>
 
-
-
-<label>
-رمز عبور
-</label>
-
+<label>رمز عبور</label>
 <input 
-class="form-control"
-name="password"
-placeholder="رمز عبور"
-required>
+    class="form-control"
+    name="password"
+    placeholder="رمز عبور"
+    required
+>
 
-
-
-
-<label>
-تاریخ انقضا
-</label>
-
-
+<label>تاریخ انقضا</label>
 <input
+    class="form-control"
+    type="date"
+    name="expire_date"
+    required
+>
 
-class="form-control"
-
-type="date"
-
-name="expire_date"
-
-required>
-
-
-
-<label>
-حجم مجاز (GB)
-</label>
-
-
+<label>حجم مجاز (GB)</label>
 <input
-
-class="form-control"
-
-name="total_gb"
-
-placeholder="مثلا 100">
-
-
-
-
+    class="form-control"
+    name="total_gb"
+    placeholder="مثلا 100"
+>
 
 <button class="form-btn">
-
-ساخت کاربر
-
+    ساخت کاربر
 </button>
-
-
 
 </form>
 
-
-
 </div>
 
-
 </div>
-
-
-
 
 <?php
-
 include "../../includes/footer.php";
-
 ?>
