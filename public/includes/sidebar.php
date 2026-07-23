8b04cd7d920841b6891f8ddc015cf2ea2c6cd4bf
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 20:12:48 +0330

    Create sidebar.php

---
+++ public/includes/sidebar.php
@@ -0,0 +1,41 @@
+<div class="sidebar">
+
+<h3>
+L-PANEL
+</h3>
+
+
+<a href="dashboard.php">
+🏠 داشبورد
+</a>
+
+
+<a href="#">
+👥 کاربران
+</a>
+
+
+<a href="#">
+👮 مدیران
+</a>
+
+
+<a href="#">
+⚙ تنظیمات
+</a>
+
+
+<a href="#">
+💾 بکاپ
+</a>
+
+
+<a href="logout.php">
+🚪 خروج
+</a>
+
+
+</div>
+
+
+<div class="content">
