0f55f2212761d6920741d534eb7d2b6b89475e6a
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 18:48:03 +0330

    Create login.php

---
+++ public/login.php
@@ -0,0 +1,126 @@
+<?php
+
+require "../app/database.php";
+
+
+$error="";
+
+
+if($_POST){
+
+
+$username=$_POST['username'];
+
+$password=$_POST['password'];
+
+
+
+$stmt=$db->prepare(
+"SELECT * FROM admins WHERE username=?"
+);
+
+
+$stmt->execute([$username]);
+
+
+$user=$stmt->fetch();
+
+
+
+if($user && password_verify($password,$user['password'])){
+
+
+$_SESSION['admin']=$user;
+
+
+header("Location: dashboard.php");
+
+exit;
+
+
+}
+
+else{
+
+
+$error="Login failed";
+
+
+}
+
+
+
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html dir="rtl">
+
+<head>
+
+<title>L-PANEL</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+</head>
+
+
+<body class="bg-dark">
+
+
+<div class="container mt-5">
+
+
+<div class="card p-4">
+
+
+<h3>
+ورود مدیر
+</h3>
+
+
+<form method="post">
+
+
+<input class="form-control mb-3"
+name="username"
+placeholder="Username">
+
+
+<input class="form-control mb-3"
+type="password"
+name="password"
+placeholder="Password">
+
+
+<button class="btn btn-primary">
+
+ورود
+
+</button>
+
+
+</form>
+
+
+<p class="text-danger">
+
+<?=$error?>
+
+</p>
+
+
+</div>
+
+
+</div>
+
+
+</body>
+
+</html>
