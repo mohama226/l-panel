82fd5256a54a258428b094e2746fcf923a0ba07d
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 19:57:46 +0330

    Update dashboard.php

--- public/dashboard.php
+++ public/dashboard.php
@@ -4,60 +4,12 @@ require "../app/auth.php";
 
 checkLogin();
 
-
 ?>
 
-
-<!DOCTYPE html>
-
-<html dir="rtl">
-
-
-<head>
-
-<title>L-PANEL</title>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
-
-
-<body>
-
-
-<div class="container mt-5">
-
-
 <h1>
-
-L-PANEL Dashboard
-
+Welcome <?php echo $_SESSION['admin']; ?>
 </h1>
 
-
-<hr>
-
-
-<h3>
-
-خوش آمدید
-
-<?=$_SESSION['admin']['username']?>
-
-</h3>
-
-
-<a href="logout.php"
-class="btn btn-danger">
-
-خروج
-
+<a href="logout.php">
+Logout
 </a>
-
-
-</div>
-
-
-</body>
-
-</html>
