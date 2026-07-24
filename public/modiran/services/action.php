a249b21e2a7da00a7aa5c1b144426dc9501cd9a6
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 01:20:05 +0330

    Update action.php

--- public/modiran/services/action.php
+++ public/modiran/services/action.php
@@ -1,29 +1,20 @@
 <?php
 
+require_once dirname(__DIR__,3)."/app/init.php";
 
-require "../../../app/auth.php";
-require "../../../app/service_manager.php";
-
+require_once BASE_PATH."/app/auth.php";
+require_once BASE_PATH."/app/service_manager.php";
 
 checkLogin();
 
-
-
-$service=$_GET['service'] ?? '';
-
-$action=$_GET['action'] ?? '';
-
-
+$service = $_GET['service'] ?? '';
+$action  = $_GET['action'] ?? '';
 
 serviceAction(
     $service,
     $action
 );
 
-
-
-header(
-"Location:index.php"
-);
-
+header("Location:index.php");
 exit;
+
