6cac3ac2da5458b5a4009e2f75ea17a3a288f360
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 01:00:16 +0330

    Update index.php

--- public/modiran/services/index.php
+++ public/modiran/services/index.php
@@ -5,141 +5,79 @@ require "../../../app/auth.php";
 require "../../../app/permissions.php";
 require "../../../app/service_manager.php";
 
-
 checkLogin();
 
-$services=getServices();
-
-echo "<pre>";
-print_r($services);
-echo "</pre>";
+$services = getServices();
 
 include "../../includes/header.php";
 include "../../includes/sidebar.php";
 
 ?>
 
-
 <div class="main">
-
 
 <h1 class="title">
 ⚙️ مدیریت سرویس‌های سرور
 </h1>
 
-
-
 <div class="panel-box">
-
 
 <table class="service-table">
 
-
 <tr>
-
-<th>
-سرویس
-</th>
-
-<th>
-وضعیت
-</th>
-
-<th>
-نوع
-</th>
-
-<th>
-عملیات
-</th>
-
+    <th>سرویس</th>
+    <th>وضعیت</th>
+    <th>نوع</th>
+    <th>عملیات</th>
 </tr>
-
-
 
 <?php foreach($services as $s): ?>
 
-
 <tr>
 
-
 <td>
-<?=htmlspecialchars($s['name'])?>
+    <?= htmlspecialchars($s['name']) ?>
 </td>
 
-
 <td>
-
-<?php if($s['active']=="active"): ?>
-
-<span class="running">
-فعال
-</span>
-
+<?php if($s['active'] == "active"): ?>
+    <span class="running">فعال</span>
 <?php else: ?>
-
-<span class="stopped">
-متوقف
-</span>
-
+    <span class="stopped">متوقف</span>
 <?php endif; ?>
-
-
 </td>
-
-
 
 <td>
-<?=$s['sub']?>
+    <?= $s['sub'] ?>
 </td>
-
-
 
 <td>
 
+    <a class="btn green"
+       href="action.php?service=<?= $s['name'] ?>&action=start">
+       Start
+    </a>
 
-<a class="btn green"
-href="action.php?service=<?=$s['name']?>&action=start">
+    <a class="btn red"
+       href="action.php?service=<?= $s['name'] ?>&action=stop">
+       Stop
+    </a>
 
-Start
-
-</a>
-
-
-<a class="btn red"
-href="action.php?service=<?=$s['name']?>&action=stop">
-
-Stop
-
-</a>
-
-
-
-<a class="btn orange"
-href="action.php?service=<?=$s['name']?>&action=restart">
-
-Restart
-
-</a>
-
+    <a class="btn orange"
+       href="action.php?service=<?= $s['name'] ?>&action=restart">
+       Restart
+    </a>
 
 </td>
-
-
 
 </tr>
 
-
 <?php endforeach; ?>
-
 
 </table>
 
-
 </div>
 
-
 </div>
-
 
 <?php include "../../includes/footer.php"; ?>
