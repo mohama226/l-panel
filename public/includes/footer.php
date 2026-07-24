e609d26d133608820db80ff1b5868127edf9ae28
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 01:12:26 +0330

    Update footer.php

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,47 +1,44 @@
-</div>
+<?php
+
+require_once __DIR__ . "/../../app/auth.php";
+
+?>
 
 </div>
 
+</div>
 
 <script>
 
 function updateClock(){
 
-    let now=new Date();
+    let now = new Date();
 
+    let time = now.toLocaleTimeString('fa-IR');
 
-    let time =
-    now.toLocaleTimeString('fa-IR');
+    let gregorian = now.toLocaleDateString('en-US');
 
+    document.getElementById("live-time").innerHTML = time;
 
-    let gregorian =
-    now.toLocaleDateString('en-US');
-
-
-    document.getElementById("live-time").innerHTML=time;
-
-    document.getElementById("gregorian-date").innerHTML=
-    "میلادی : "+gregorian;
-
+    document.getElementById("gregorian-date").innerHTML =
+        "میلادی : " + gregorian;
 
     let shamsi =
-    new Intl.DateTimeFormat(
-        'fa-IR-u-ca-persian',
-        {
-            year:'numeric',
-            month:'long',
-            day:'numeric'
-        }
-    ).format(now);
+        new Intl.DateTimeFormat(
+            'fa-IR-u-ca-persian',
+            {
+                year:'numeric',
+                month:'long',
+                day:'numeric'
+            }
+        ).format(now);
 
-
-    document.getElementById("shamsi-date").innerHTML=
-    "شمسی : "+shamsi;
+    document.getElementById("shamsi-date").innerHTML =
+        "شمسی : " + shamsi;
 
 }
 
-
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
 
 updateClock();
 
