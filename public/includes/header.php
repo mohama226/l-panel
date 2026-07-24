6bc5909efb8d73323fb7d6bea8e9ce0bc0b590be
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 19:27:13 +0330

    Update header.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,29 +1,17 @@
-<?php
-?>
-
 <!DOCTYPE html>
-
 <html lang="fa" dir="rtl">
 
 <head>
 
 <meta charset="UTF-8">
 
-<title>
-L-PANEL | مدیریت VPN
-</title>
+<title>L-PANEL</title>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<link href="https://cdn.jsdelivr.net/npm/vazirmatn@latest/misc/UI-Farsi-Digits/Vazirmatn-UI-FD-font-face.css" rel="stylesheet">
 
 <link rel="stylesheet" href="/assets/css/style.css">
-<link rel="stylesheet" href="/assets/css/dropdown.css">
-<link rel="stylesheet" href="/assets/css/users.css">
-<link rel="stylesheet" href="/assets/css/forms.css">  
-<link rel="stylesheet" href="/assets/css/admin.css">
-<link rel="stylesheet" href="/assets/css/forms.css">
-  
+
+
 </head>
+
 
 <body>
