aae331f374a0b0df3c617d3c45834325331ba6cf
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 20:24:07 +0330

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,40 +1,38 @@
 CREATE TABLE IF NOT EXISTS admins (
 
-id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
 
-username VARCHAR(50) UNIQUE NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
 
-password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
 
-role ENUM(
-'superadmin',
-'admin'
-)
-DEFAULT 'admin',
+    role ENUM(
+        'superadmin',
+        'admin'
+    ) DEFAULT 'admin',
 
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    status ENUM(
+        'active',
+        'disabled'
+    ) DEFAULT 'active',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 );
 
 
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS admin_permissions (
 
-id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
 
-username VARCHAR(100) UNIQUE NOT NULL,
+    admin_id INT NOT NULL,
 
-password VARCHAR(255) NOT NULL,
+    permission VARCHAR(100) NOT NULL,
 
-status ENUM('active','blocked') DEFAULT 'active',
-
-expire_date DATE NULL,
-
-total_gb INT DEFAULT 0,
-
-used_gb INT DEFAULT 0,
-
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY(admin_id)
+    REFERENCES admins(id)
+    ON DELETE CASCADE
 
 );
 
@@ -42,16 +40,37 @@ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 CREATE TABLE IF NOT EXISTS admin_logs (
 
-id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
 
-admin_id INT,
+    admin VARCHAR(100),
 
-action VARCHAR(100),
+    action VARCHAR(255),
 
-description TEXT,
+    ip VARCHAR(50),
 
-ip VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE IF NOT EXISTS users (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    username VARCHAR(100) UNIQUE,
+
+    password VARCHAR(255),
+
+    expire_date DATE,
+
+    status ENUM(
+        'active',
+        'blocked'
+    ) DEFAULT 'active',
+
+    created_by INT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 );
