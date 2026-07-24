cf2c8ed74951f89e6bee48545a6d0e1cde91ac43
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 00:55:09 +0330

    Update header.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,29 +1,22 @@
 <!DOCTYPE html>
-
 <html lang="fa" dir="rtl">
-
 
 <head>
 
 <meta charset="UTF-8">
-
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
+<title>L-PANEL</title>
 
-<title>
-L-PANEL
-</title>
-
-<link rel="stylesheet" href="/assets/css/services.css">
-<link rel="stylesheet" href="/assets/css/admin.css">
+<!-- ترتیب صحیح و بدون تداخل -->
 <link rel="stylesheet" href="/assets/css/style.css">
+<link rel="stylesheet" href="/assets/css/admin.css">
+<link rel="stylesheet" href="/assets/css/services.css">
+
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  
+
 </head>
 
-
 <body>
-
-
 
 <div class="layout">
