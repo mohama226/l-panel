dd8a9c9b6113d12f49223853889e025b494b28a3
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 19:40:14 +0330

    Update header.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,6 +1,3 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
 
@@ -8,10 +5,17 @@
 
 <meta charset="UTF-8">
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title>L-PANEL</title>
+
 
 <link rel="stylesheet" href="/assets/css/style.css">
 
 </head>
 
+
 <body>
+
+
+<div class="layout">
