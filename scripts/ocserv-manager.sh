9ecda63eb6964a63ca1ec28fa8eecfbe4a16a5ff
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 10:39:42 +0330

    Update ocserv-manager.sh

--- scripts/ocserv-manager.sh
+++ scripts/ocserv-manager.sh
@@ -4,45 +4,38 @@ ACTION=$1
 USERNAME=$2
 PASSWORD=$3
 
-
 OCFILE="/etc/ocserv/ocpasswd"
 
 
 case "$ACTION" in
 
-
 add)
 
-echo "$PASSWORD" | ocpasswd -c "$OCFILE" "$USERNAME"
+echo "$PASSWORD" | /usr/bin/ocpasswd -c "$OCFILE" "$USERNAME"
 
-systemctl restart ocserv
+/usr/bin/systemctl restart ocserv
 
 ;;
-
 
 delete)
 
-ocpasswd -c "$OCFILE" -d "$USERNAME"
+ /usr/bin/ocpasswd -c "$OCFILE" -d "$USERNAME"
 
-systemctl restart ocserv
+/usr/bin/systemctl restart ocserv
 
 ;;
-
 
 restart)
 
-systemctl restart ocserv
+/usr/bin/systemctl restart ocserv
 
 ;;
-
 
 *)
 
 echo "Invalid action"
-
 exit 1
 
 ;;
-
 
 esac
