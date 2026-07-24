775f875cccbbde8b47d261d0fc55fb82f20c606c
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 20:02:26 +0330

    Update sidebar.php

--- public/includes/sidebar.php
+++ public/includes/sidebar.php
@@ -1,50 +1,67 @@
+<?php
+
+require_once "../../app/auth.php";
+
+?>
+
+
 <div class="sidebar">
 
-    <div class="logo">
-        L-PANEL
-    </div>
 
-    <a href="/modiran/dashboard.php">
-        داشبورد
-    </a>
-
-    <a href="/users/">
-        کاربران VPN
-    </a>
-
-    <a href="/modiran/logs.php">
-        لاگ سیستم
-    </a>
-
-    <a href="/logout.php">
-        خروج
-    </a>
-
-</div>
+<h2>
+L-PANEL
+</h2>
 
 
-<div class="content">
+<a href="/modiran/dashboard.php">
+داشبورد
+</a>
+
+
+
+<a href="/modiran/users/">
+کاربران VPN
+</a>
+
+
+
 
 <?php if(isSuperAdmin()): ?>
 
-    <li>
-        <a href="/modiran/manage/">
-            مدیریت مدیران
-        </a>
-    </li>
 
-    <li>
-        <a href="/modiran/settings/panel.php">
-            تنظیمات پنل
-        </a>
-    </li>
+<hr>
 
-    <li>
-        <a href="/modiran/settings/backup.php">
-            بکاپ
-        </a>
-    </li>
+
+<h4>
+مدیریت سیستم
+</h4>
+
+
+<a href="/modiran/manage/">
+مدیران
+</a>
+
+
+
+<a href="/modiran/settings/panel.php">
+تنظیمات
+</a>
+
+
+
+<a href="/modiran/logs.php">
+لاگ مدیران
+</a>
+
+
 
 <?php endif; ?>
+
+
+<a href="/modiran/logout.php">
+خروج
+</a>
+
+
 
 </div>
