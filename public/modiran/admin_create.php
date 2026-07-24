c815c3619152e66b155926cfae05be53217a133c
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 22:49:13 +0330

    Update admin_create.php

--- public/modiran/admin_create.php
+++ public/modiran/admin_create.php
@@ -1,57 +1,116 @@
 <?php
 
+
 require "../../app/database.php";
 require "../../app/auth.php";
+
 
 checkLogin();
 requireSuperAdmin();
 
+
+
 if($_POST){
 
-    $username = $_POST['username'];
 
-    $password = password_hash(
-        $_POST['password'],
-        PASSWORD_DEFAULT
-    );
+$username=$_POST['username'];
 
-    $stmt = $db->prepare("
-        INSERT INTO admins
-        (username, password, role)
-        VALUES (?,?,?)
-    ");
+$password=password_hash(
+$_POST['password'],
+PASSWORD_DEFAULT
+);
 
-    $stmt->execute([
-        $username,
-        $password,
-        $_POST['role']
-    ]);
 
-    echo "Admin Created";
+
+$stmt=$db->prepare(
+
+"
+INSERT INTO admins
+(username,password,role,status)
+VALUES(?,?,?,?)
+"
+
+);
+
+
+
+$stmt->execute([
+
+$username,
+$password,
+$_POST['role'],
+'active'
+
+]);
+
+
+
+header(
+"Location: admins.php"
+);
+
+exit;
+
+
 }
+
+
+
+include "../includes/header.php";
+
 
 ?>
 
+
+<div class="container">
+
+
+<h2>
+ساخت مدیر جدید
+</h2>
+
+
+
 <form method="post">
 
-<input name="username" placeholder="username">
 
-<input name="password" placeholder="password">
+<input name="username" placeholder="Username">
+
+
+<input 
+type="password"
+name="password"
+placeholder="Password"
+>
+
+
 
 <select name="role">
+
 
 <option value="admin">
 Admin
 </option>
 
+
 <option value="superadmin">
 Super Admin
 </option>
 
+
 </select>
 
+
+
 <button>
-Create
+ذخیره
 </button>
 
+
 </form>
+
+
+</div>
+
+
+<?php include "../includes/footer.php"; ?>
