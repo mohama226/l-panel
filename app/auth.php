c482e96dd47a8d6b65fbbf400c6d807c36d1e1f3
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 20:34:21 +0330

    Update auth.php

--- app/auth.php
+++ app/auth.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__."/session.php";
+
+session_start();
+
 
 
 function checkLogin(){
@@ -22,88 +24,5 @@ exit;
 }
 
 
-
-function isSuperAdmin(){
-
-
-return (
-
-isset($_SESSION['role'])
-
-&&
-
-$_SESSION['role']=="superadmin"
-
-);
-
-
-}
-
-
-
-function requireSuperAdmin(){
-
-
-if(!isSuperAdmin()){
-
-
-die(
-"Access Denied"
-);
-
-
-}
-
-
-}
-
-
-
-function hasPermission($permission){
-
-
-global $db;
-
-
-if(isSuperAdmin()){
-
-return true;
-
-}
-
-
-
-$stmt=$db->prepare(
-"
-SELECT COUNT(*)
-FROM admin_permissions p
-
-JOIN admins a
-
-ON a.id=p.admin_id
-
-WHERE a.username=?
-
-AND p.permission=?
-
-"
-);
-
-
-
-$stmt->execute([
-
-$_SESSION['admin'],
-
-$permission
-
-]);
-
-
-
-return $stmt->fetchColumn()>0;
-
-
-}
 
 ?>
