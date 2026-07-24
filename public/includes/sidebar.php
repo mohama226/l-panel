226d6c2de4e69055ef2142fd181c09517a47a609
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 01:26:20 +0330

    Update sidebar.php

--- public/includes/sidebar.php
+++ public/includes/sidebar.php
@@ -4,6 +4,7 @@ require_once dirname(__DIR__,2)."/app/init.php";
 require_once BASE_PATH."/app/auth.php";
 
 ?>
+
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
 
@@ -14,7 +15,6 @@ require_once BASE_PATH."/app/auth.php";
 
 <title>L-PANEL</title>
 
-<!-- ترتیب صحیح و بدون تداخل -->
 <link rel="stylesheet" href="/assets/css/style.css">
 <link rel="stylesheet" href="/assets/css/admin.css">
 <link rel="stylesheet" href="/assets/css/services.css">
@@ -23,6 +23,66 @@ require_once BASE_PATH."/app/auth.php";
 
 </head>
 
+
 <body>
 
+
 <div class="layout">
+
+
+<aside class="sidebar">
+
+
+<h2>
+L-PANEL
+</h2>
+
+
+
+<a href="/modiran/dashboard.php">
+🏠 داشبورد
+</a>
+
+
+
+<a href="/modiran/users/">
+👥 کاربران VPN
+</a>
+
+
+
+<hr>
+
+
+
+<h3>
+مدیریت سیستم
+</h3>
+
+
+
+<a href="/modiran/admins.php">
+👑 مدیران
+</a>
+
+
+
+<a href="/modiran/logs.php">
+📋 لاگ مدیران
+</a>
+
+
+
+<a href="/modiran/services/">
+⚙️ تنظیمات سرویس‌ها
+</a>
+
+
+
+<a href="/modiran/logout.php">
+🚪 خروج
+</a>
+
+
+
+</aside>
