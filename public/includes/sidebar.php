e7141725589c4432e069ab433cb831516fd80184
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 19:59:24 +0330

    Update sidebar.php

--- public/includes/sidebar.php
+++ public/includes/sidebar.php
@@ -1,35 +1,50 @@
 <div class="sidebar">
 
+    <div class="logo">
+        L-PANEL
+    </div>
 
-<div class="logo">
+    <a href="/modiran/dashboard.php">
+        داشبورد
+    </a>
 
-L-PANEL
+    <a href="/users/">
+        کاربران VPN
+    </a>
 
-</div>
+    <a href="/modiran/logs.php">
+        لاگ سیستم
+    </a>
 
-
-
-<a href="/modiran/dashboard.php">
-داشبورد
-</a>
-
-
-<a href="/users/">
-کاربران VPN
-</a>
-
-
-<a href="/modiran/logs.php">
-لاگ سیستم
-</a>
-
-
-<a href="/logout.php">
-خروج
-</a>
-
+    <a href="/logout.php">
+        خروج
+    </a>
 
 </div>
 
 
 <div class="content">
+
+<?php if(isSuperAdmin()): ?>
+
+    <li>
+        <a href="/modiran/manage/">
+            مدیریت مدیران
+        </a>
+    </li>
+
+    <li>
+        <a href="/modiran/settings/panel.php">
+            تنظیمات پنل
+        </a>
+    </li>
+
+    <li>
+        <a href="/modiran/settings/backup.php">
+            بکاپ
+        </a>
+    </li>
+
+<?php endif; ?>
+
+</div>
