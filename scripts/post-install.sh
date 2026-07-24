8686ca93543c36e3b167e8c93563827c749115c7
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 12:18:42 +0330

    Update post-install.sh

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,27 +2,26 @@
 
 echo "Configuring L-PANEL OCServ..."
 
-
 mkdir -p /etc/ocserv
-
 
 touch /etc/ocserv/ocpasswd
 
-chown root:apache /etc/ocserv/ocpasswd
-chmod 660 /etc/ocserv/ocpasswd
+chown root:root /etc/ocserv
+chmod 755 /etc/ocserv
+
+chown root:root /etc/ocserv/ocpasswd
+chmod 600 /etc/ocserv/ocpasswd
 
 
 chmod +x /var/www/html/l-panel/scripts/*.sh
 
 
 cat >/etc/sudoers.d/l-panel-ocserv <<EOF
-
 apache ALL=(root) NOPASSWD: /var/www/html/l-panel/scripts/ocserv-manager.sh
-
 EOF
 
 
 chmod 440 /etc/sudoers.d/l-panel-ocserv
 
 
-echo "OCServ integration ready"
+echo "L-PANEL OCServ permissions configured"
