fe79a9d0fadc2d25c0c47131cead661a8c41bc5f
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 22:47:20 +0330

    Update permission.php

--- app/permission.php
+++ app/permission.php
@@ -1,57 +1,75 @@
 <?php
 
 
-function isSuperAdmin(){
-
-    return isset($_SESSION['role'])
-    &&
-    $_SESSION['role']=="superadmin";
-
-}
-
-
-
-function requireSuperAdmin(){
-
-    if(!isSuperAdmin()){
-
-        die("Access Denied");
-
-    }
-
-}
-
-
-
 function hasPermission($permission){
 
 
-    if(isSuperAdmin()){
-
-        return true;
-
-    }
+global $db;
 
 
-    if(!isset($_SESSION['permissions'])){
+if(
+isset($_SESSION['role'])
+&&
+$_SESSION['role']=="superadmin"
+){
 
-        return false;
+    return true;
 
-    }
-
-
-    $list=json_decode(
-        $_SESSION['permissions'],
-        true
-    );
+}
 
 
-    return in_array(
-        $permission,
-        $list
-    );
+
+if(!isset($_SESSION['admin_id'])){
+
+    return false;
+
+}
+
+
+
+$stmt=$db->prepare(
+
+"
+SELECT COUNT(*)
+FROM admin_permissions
+WHERE admin_id=?
+AND permission=?
+"
+
+);
+
+
+
+$stmt->execute([
+
+$_SESSION['admin_id'],
+$permission
+
+]);
+
+
+
+return $stmt->fetchColumn()>0;
+
 
 
 }
+
+
+
+function requirePermission($permission){
+
+
+if(!hasPermission($permission)){
+
+
+die("Permission Denied");
+
+
+}
+
+
+}
+
 
 ?>
