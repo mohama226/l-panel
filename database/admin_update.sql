6bd160a6a5fc139f99b823bf48da1223da107077
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 22:48:27 +0330

    Update admin_update.sql

--- database/admin_update.sql
+++ database/admin_update.sql
@@ -6,10 +6,18 @@ admin_id INT NOT NULL,
 
 permission VARCHAR(100) NOT NULL,
 
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
 FOREIGN KEY(admin_id)
 REFERENCES admins(id)
 ON DELETE CASCADE
 
 );
+
+
+
+INSERT INTO admin_permissions
+(admin_id,permission)
+
+SELECT id,'users_manage'
+FROM admins
+WHERE role='superadmin';
