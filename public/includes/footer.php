abc37586a3ba342f66b233abe113b362860ef3b0
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 00:31:38 +0330

    Update footer.php

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -3,6 +3,50 @@
 </div>
 
 
+<script>
+
+function updateClock(){
+
+    let now=new Date();
+
+
+    let time =
+    now.toLocaleTimeString('fa-IR');
+
+
+    let gregorian =
+    now.toLocaleDateString('en-US');
+
+
+    document.getElementById("live-time").innerHTML=time;
+
+    document.getElementById("gregorian-date").innerHTML=
+    "میلادی : "+gregorian;
+
+
+    let shamsi =
+    new Intl.DateTimeFormat(
+        'fa-IR-u-ca-persian',
+        {
+            year:'numeric',
+            month:'long',
+            day:'numeric'
+        }
+    ).format(now);
+
+
+    document.getElementById("shamsi-date").innerHTML=
+    "شمسی : "+shamsi;
+
+}
+
+
+setInterval(updateClock,1000);
+
+updateClock();
+
+</script>
+
 </body>
 
 </html>
