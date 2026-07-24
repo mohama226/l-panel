338ddb61309697a9b6da8364afe21d8a0558e8b6
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 19:39:28 +0330

    Update index.php

--- public/modiran/index.php
+++ public/modiran/index.php
@@ -55,8 +55,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <meta charset="UTF-8">
 <title>L-PANEL Admin</title>
 
-<link rel="stylesheet" href="../assets/css/login.css">
-
+<link rel="stylesheet" href="/assets/css/login.css">
 </head>
 
 
