06add2b5dae6a12a3295fba66420c5e35031723e
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 18:21:32 +0330

    Update footer.php

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,46 +1,8 @@
-<?php
-?>
-
-</div>
+</div> <!-- پایان layout -->
 
 <footer class="footer">
-
-
+    <p>L-PANEL © 2026</p>
 </footer>
 
-
-<script>
-
-
-// حذف ساعت و تاریخ سایدبار
-document.addEventListener("DOMContentLoaded", function(){
-
-    let clock = document.getElementById("live-time");
-    let gdate = document.getElementById("gregorian-date");
-    let sdate = document.getElementById("shamsi-date");
-
-
-    if(clock){
-        clock.remove();
-    }
-
-
-    if(gdate){
-        gdate.remove();
-    }
-
-
-    if(sdate){
-        sdate.remove();
-    }
-
-
-});
-
-
-</script>
-
-
 </body>
-
 </html>
