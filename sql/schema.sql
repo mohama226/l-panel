3d0220674c0d7434c6b5535a7acf142405a40a09
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 01:42:02 +0330

    Delete sql directory

--- sql/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS admins (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    role ENUM('superadmin','admin','blocked') DEFAULT 'admin',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
