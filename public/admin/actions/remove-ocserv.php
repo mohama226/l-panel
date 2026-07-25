a8cae1a9b3e580144a79d044139f075c1257859a
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 22:49:33 +0330

    Update remove-ocserv.php

--- public/admin/actions/remove-ocserv.php
+++ public/admin/actions/remove-ocserv.php
@@ -1,1 +1,10 @@
 
+<?php
+
+shell_exec("systemctl stop ocserv");
+shell_exec("systemctl disable ocserv");
+shell_exec("dnf remove -y ocserv");
+
+header("Location: ../ocserv-install.php?status=removed");
+exit;
+?>
