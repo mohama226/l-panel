4be8b3eaeeb75f5589fc281b6544983acfe235f6
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 22:17:39 +0330

    Update login-reseller.php

--- public/login-reseller.php
+++ public/login-reseller.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
-<html>
+<html lang="fa">
 <head>
     <meta charset="UTF-8">
     <title>Reseller Login</title>
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
-<div class="login-container">
-    <div class="login-title">Reseller Login</div>
 
-    <form method="post">
-        <label>Username</label>
-        <input type="text" required>
+<div class="login-box">
+    <h2>Reseller Login</h2>
 
-        <label>Password</label>
-        <input type="password" required>
+    <form method="POST" action="auth-reseller.php">
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username" required>
+        </div>
 
-        <button type="submit">Login</button>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password" required>
+        </div>
+
+        <button class="btn">Login</button>
     </form>
+
+    <div class="switch-link">
+        <a href="login-admin.php">Admin Login</a> |
+        <a href="login-user.php">User Login</a>
+    </div>
 </div>
+
 </body>
 </html>
