0054e6163aeb18404a3ce93d051dda4958c6b077
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 19:07:59 +0330

    Delete public directory

--- public/includes/footer.php
+++
@@ -1,8 +0,0 @@
-</div> <!-- پایان layout -->
-
-<footer class="footer">
-    <p>L-PANEL © 2026</p>
-</footer>
-
-</body>
-</html>
--- public/modiran/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once dirname(__DIR__,2) . "/app/init.php";
-require_once BASE_PATH . "/app/auth.php";
-
-checkLogin();
-
-header("Location: /modiran/dashboard.php");
-exit;
