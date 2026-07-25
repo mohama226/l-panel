09ec09859244f1feb0ef705ad93aeb3c08a619d9
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 18:43:09 +0330

    Update index.php

--- public/modiran/index.php
+++ public/modiran/index.php
@@ -3,9 +3,7 @@
 require_once dirname(__DIR__,2) . "/app/init.php";
 require_once BASE_PATH . "/app/auth.php";
 
-// بررسی لاگین
 checkLogin();
 
-// هدایت به داشبورد
 header("Location: dashboard.php");
 exit;
