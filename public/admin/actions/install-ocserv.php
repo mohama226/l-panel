9a920472927e1683c9b0eff46971be1ddae2edac
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 23:02:44 +0330

    Update install-ocserv.php

--- public/admin/actions/install-ocserv.php
+++ public/admin/actions/install-ocserv.php
@@ -1,26 +1,20 @@
-
 <?php
 
 $port = intval($_POST['port']);
 
-// نصب ocserv
 shell_exec("dnf install -y epel-release");
 shell_exec("dnf install -y ocserv");
 
-// توقف سرویس قبل از کانفیگ
 shell_exec("systemctl stop ocserv");
 
-// دانلود کانفیگ دلخواه از GitHub
-$config_url = "https://raw.githubusercontent.com/mohama226/l-panel/main/php-panel/configs/ocserv.conf";
+$config_url = "https://raw.githubusercontent.com/mohama226/l-panel/main/public/configs/ocserv.conf";
 $config_path = "/etc/ocserv/ocserv.conf";
 
 shell_exec("curl -s $config_url -o $config_path");
 
-// تنظیم پورت
 shell_exec("sed -i 's/^tcp-port = .*/tcp-port = $port/' $config_path");
 shell_exec("sed -i 's/^udp-port = .*/udp-port = $port/' $config_path");
 
-// فعال‌سازی سرویس
 shell_exec("systemctl enable ocserv");
 shell_exec("systemctl start ocserv");
 
