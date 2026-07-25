6bc7eeeb5727fa94e2318add17f1e757eb52ef61
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 10:17:50 +0330

    Update admin_permissions.sql

--- database/admin_permissions.sql
+++ database/admin_permissions.sql
@@ -1,24 +1,35 @@
-CREATE TABLE IF NOT EXISTS permissions(
+CREATE TABLE IF NOT EXISTS permissions (
 
-id int AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 
-name varchar(100),
+name VARCHAR(100) NOT NULL,
 
-title varchar(150)
+title VARCHAR(150) NOT NULL
 
 );
 
 
-INSERT INTO permissions(name,title) VALUES
+
+INSERT INTO permissions
+(name,title)
+VALUES
 
 ('users_view','مشاهده کاربران'),
-('users_create','ایجاد کاربر'),
-('users_edit','ویرایش کاربر'),
-('users_delete','حذف کاربر'),
-('users_lock','لاک کردن کاربر'),
-('users_unlock','باز کردن کاربر'),
-('services_manage','مدیریت سرویس ها'),
-('logs_view','مشاهده لاگ ها'),
-('backup_manage','مدیریت بکاپ'),
-('settings_manage','تنظیمات پنل');
 
+('users_create','ایجاد کاربر'),
+
+('users_edit','ویرایش کاربر'),
+
+('users_delete','حذف کاربر'),
+
+('users_lock','لاک کردن کاربر'),
+
+('users_unlock','باز کردن کاربر'),
+
+('services_manage','مدیریت سرویس ها'),
+
+('logs_view','مشاهده لاگ ها'),
+
+('backup_manage','مدیریت بکاپ'),
+
+('settings_manage','تنظیمات پنل');
