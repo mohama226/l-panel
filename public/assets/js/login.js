81e3ceab5ec404e69f256d1c1f38e6c53a79633d
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 02:44:58 +0330

    Delete public directory

--- public/assets/js/login.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const box = document.querySelector(".login-box");
-    box.style.transform = "scale(0.95)";
-    setTimeout(() => {
-        box.style.transition = "0.4s";
-        box.style.transform = "scale(1)";
-    }, 150);
-});
