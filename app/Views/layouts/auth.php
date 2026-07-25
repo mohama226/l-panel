ac6b1cfe368b68e15128fbe8dd59b0ed64d8e130
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 02:46:55 +0330

    Create auth.php

---
+++ app/Views/layouts/auth.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fa" dir="rtl">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title><?= $title ?? 'L-Panel'; ?></title>
+
+<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="/assets/css/auth.css">
+
+</head>
+
+<body>
+
+<?= $content ?>
+
+<script src="/assets/js/auth.js"></script>
+
+</body>
+
+</html>
