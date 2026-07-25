2e60ff9f7f0b87d21935e19c3237f56ef58c4c1d
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-25 19:37:43 +0330

    Update install.sh

--- php-panel/install/install.sh
+++ php-panel/install/install.sh
@@ -67,7 +67,8 @@ echo "[*] Creating database and user..."
 
 mysql -u root <<EOF
 DROP USER IF EXISTS '${DB_USER}'@'localhost';
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+DROP DATABASE IF EXISTS ${DB_NAME};
+CREATE DATABASE ${DB_NAME};
 CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
 FLUSH PRIVILEGES;
