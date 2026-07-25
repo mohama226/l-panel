200b2a18782d8ff612a4e16efc8727da3bea38f7
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 01:42:06 +0330

    Delete public directory

--- public/admin/actions/remove-ocserv.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-shell_exec("systemctl stop ocserv");
-shell_exec("systemctl disable ocserv");
-shell_exec("dnf remove -y ocserv");
-
-header("Location: ../ocserv-install.php?status=removed");
-exit;
-?>
