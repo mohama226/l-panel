2143e0f865e08304bfe18c4672f86be87097f79f
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 23:27:45 +0330

    Update nginx.sh

--- installer/nginx.sh
+++ installer/nginx.sh
@@ -1,57 +1,53 @@
 #!/bin/bash
 
 
-install_nginx(){
-
-echo "[OK] Configuring Nginx"
-
+configure_nginx(){
 
 cat > /etc/nginx/conf.d/l-panel.conf <<EOF
 
 server {
 
-    listen 2096;
+listen 2096;
 
-    server_name _;
+server_name _;
 
-    root /opt/l-panel/public;
+root /opt/l-panel/public;
 
-    index index.php index.html;
-
-
-    location / {
-
-        try_files \$uri \$uri/ /index.php?\$query_string;
-
-    }
+index index.php index.html;
 
 
-    location ~ \.php$ {
+location / {
 
-        include fastcgi_params;
+try_files \$uri \$uri/ /index.php?\$query_string;
 
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-
-        fastcgi_pass unix:/run/php-fpm/www.sock;
-
-    }
+}
 
 
-    location ~ /\. {
+location ~ \.php$ {
 
-        deny all;
+include fastcgi_params;
 
-    }
+fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+
+fastcgi_pass unix:/run/php-fpm/www.sock;
+
+}
+
+
+location ~ /\. {
+
+deny all;
+
+}
 
 }
 
 EOF
 
 
-systemctl enable nginx
+nginx -t
 
 systemctl restart nginx
-
 
 echo "[OK] Nginx configured"
 
