dff6dac5039b9d36a1e660f7b0c682f7a5b174a8
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 18:35:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,38 @@
 
 set -e
 
-REPO="https://raw.githubusercontent.com/mohama226/l-panel/main"
 
-bash <(curl -fsSL "$REPO/installer/install.sh")
+REPO="https://github.com/mohama226/l-panel.git"
+
+INSTALL_DIR="/opt/l-panel"
+
+
+echo "Installing L-Panel"
+
+
+if [ -d "$INSTALL_DIR/.git" ]; then
+
+echo "Updating existing installation"
+
+cd $INSTALL_DIR
+
+git pull
+
+else
+
+
+rm -rf $INSTALL_DIR
+
+git clone $REPO $INSTALL_DIR
+
+
+fi
+
+
+cd $INSTALL_DIR
+
+
+chmod +x installer/*.sh
+
+
+bash installer/main.sh
