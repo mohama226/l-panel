bd4cdd7934a202fc167beb34c4c99d8a3608b620
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 17:59:49 +0330

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'host'=>'127.0.0.1',
+
+    'port'=>3306,
+
+    'database'=>'lpanel',
+
+    'username'=>'lpanel',
+
+    'password'=>'',
+
+    'charset'=>'utf8mb4',
+
+];
