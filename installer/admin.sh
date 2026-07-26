6a224014d7c2741cf22e8b6604032b17b7377e99
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 23:23:58 +0330

    Update admin.sh

--- installer/admin.sh
+++ installer/admin.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
-
 create_admin(){
 
-
-cd /var/www/l-panel
-
-
-read -p "Super Admin username: " ADMIN
-
-
-read -s -p "Password: " PASS
-
 echo ""
+echo "Create Super Admin"
+echo ""
+
+read -p "Username: " ADMIN_USER
+
+while true
+do
+
+read -s -p "Password: " ADMIN_PASS
+echo ""
+
+read -s -p "Confirm Password: " ADMIN_PASS2
+echo ""
+
+if [ "$ADMIN_PASS" = "$ADMIN_PASS2" ]
+then
+break
+fi
+
+echo "Passwords do not match"
+
+done
+
+
+cd /opt/l-panel
 
 
 php artisan tinker --execute="
-
 \App\Models\User::create([
-
-'name'=>'$ADMIN',
-
-'email'=>'$ADMIN@admin.local',
-
-'password'=>bcrypt('$PASS')
-
+'name'=>'$ADMIN_USER',
+'email'=>'$ADMIN_USER@example.com',
+'password'=>bcrypt('$ADMIN_PASS'),
 ]);
-
 "
 
 
-success "Admin created"
-
+echo "[OK] Super Admin created"
 
 }
