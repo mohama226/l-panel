7424919da50285604abfa7e85f0bb19f65d5ee56
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 23:39:40 +0330

    la Update database.sh

--- installer/database.sh
+++ installer/database.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+create_database(){
 
-setup_database(){
+echo "[OK] Database migration"
 
-cd /opt/l-panel
-
+cd /opt/l-panel || exit 1
 
 php artisan migrate --force
 
+if [ $? -eq 0 ]; then
+    echo "[OK] Database ready"
+else
+    echo "[ERROR] Migration failed"
+    exit 1
+fi
 
 }
