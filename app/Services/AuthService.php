c52514bfe0e8247edb77eb5042fb7feedfdc9ea3
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 18:10:44 +0330

    Update AuthService.php

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -1,8 +1,61 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
+
+
+use App\Repositories\AdminRepository;
+use App\Core\Auth;
+
 
 class AuthService
 {
+
+
+    public function __construct(
+        private AdminRepository $admins
+    )
+    {}
+
+
+
+    public function attempt(
+        string $username,
+        string $password
+    ):bool
+    {
+
+
+        $admin =
+        $this->admins
+        ->findByUsername($username);
+
+
+
+        if(!$admin)
+        {
+            return false;
+        }
+
+
+
+        if(!password_verify(
+            $password,
+            $admin['password']
+        ))
+        {
+            return false;
+        }
+
+
+
+        Auth::login($admin);
+
+
+        return true;
+
+    }
+
 
 }
