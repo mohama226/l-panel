41633b801a398cd6c934bd5d1cd976761dcade6c
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 00:14:30 +0330

    Create DashboardController.php

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class DashboardController extends Controller
+{
+
+
+    public function index()
+    {
+
+        return view('dashboard');
+
+    }
+
+
+}
