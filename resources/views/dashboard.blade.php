49850660d11f033e0fbd5ea0978ac681df638a68
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 01:10:53 +0330

    la Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,47 +1,8 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-<title>L-PANEL</title>
-</head>
-
-
-<body>
-
-
 <h1>
-Welcome to L-PANEL
-</h1>
-
-
-</body>
-
-</html><!DOCTYPE html>
-<html>
-<body>
-
-
-<h1>
-L-Panel Dashboard
+L-PANEL Dashboard
 </h1>
 
 
 <p>
 Welcome Admin
 </p>
-
-
-<form method="POST" action="/logout">
-
-@csrf
-
-<button>
-Logout
-</button>
-
-
-</form>
-
-
-</body>
-</html>
