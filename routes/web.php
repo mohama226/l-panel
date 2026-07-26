1e7bb5b638a2d0ac3b25b6c9038b894e6b37f82c
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 00:50:41 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,34 +5,25 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
 
-Route::get('/', function () {
+
+Route::get('/login',[AuthController::class,'login'])
+->name('login');
+
+
+Route::post('/login',[AuthController::class,'authenticate']);
+
+
+Route::get('/dashboard',[DashboardController::class,'index'])
+->middleware('auth');
+
+
+
+Route::post('/logout',[AuthController::class,'logout']);
+
+
+
+Route::get('/',function(){
+
     return redirect('/login');
-});
-
-
-Route::get('/login', [
-    AuthController::class,
-    'show'
-])->name('login');
-
-
-Route::post('/login', [
-    AuthController::class,
-    'login'
-]);
-
-
-Route::post('/logout', [
-    AuthController::class,
-    'logout'
-]);
-
-
-Route::middleware('auth')->group(function () {
-
-    Route::get('/dashboard', [
-        DashboardController::class,
-        'index'
-    ]);
 
 });
