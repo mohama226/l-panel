879da7b4e1b410835d18adb678d3dac950456c20
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 18:22:34 +0330

    Update login.php

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,34 +1,49 @@
 <?php
 
 ob_start();
+
 ?>
 
-<h2>L-Panel Login</h2>
 
-<form method="post">
+<h2>
+L-Panel Login
+</h2>
+
+
+<form method="POST" action="/login">
+
+
+<input 
+type="hidden"
+name="_token"
+value="<?=$csrf?>">
+
 
 <input
-type="text"
-placeholder="Username"
-name="username">
+name="username"
+placeholder="Username">
+
 
 <input
 type="password"
-placeholder="Password"
-name="password">
+name="password"
+placeholder="Password">
+
 
 <button>
-
 Login
-
 </button>
+
 
 </form>
 
+
 <?php
+
 
 $content=ob_get_clean();
 
-$title='Login';
+$title="Login";
+
 
 require ROOT_PATH.'/app/Views/layouts/main.php';
