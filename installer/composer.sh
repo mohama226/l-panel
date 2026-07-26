f7659ac29e76049a1fde01852bc427b8fec70a3a
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 23:32:52 +0330

    Update composer.sh

--- installer/composer.sh
+++ installer/composer.sh
@@ -3,24 +3,54 @@
 
 install_composer(){
 
-
-if command -v composer >/dev/null
-then
-
-success "Composer exists"
-
-else
-
-
-curl -sS https://getcomposer.org/installer | php
-
-mv composer.phar /usr/local/bin/composer
+    if command -v composer >/dev/null 2>&1
+    then
+        success "Composer exists"
+        composer --version
+        return
+    fi
 
 
-fi
+    cd /tmp
 
 
-composer -V
+    php -r "copy('https://getcomposer.org/installer','composer-setup.php');"
 
+
+    php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+
+    rm -f composer-setup.php
+
+
+    success "Composer installed"
+
+}
+
+
+
+install_dependencies(){
+
+    cd /opt/l-panel
+
+
+    if [ -f composer.json ]
+    then
+
+        composer install \
+        --no-interaction \
+        --prefer-dist \
+        --optimize-autoloader
+
+
+        success "Laravel dependencies installed"
+
+    else
+
+        error "composer.json not found"
+
+        exit 1
+
+    fi
 
 }
