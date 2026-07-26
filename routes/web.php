e8168950888e0985327324ecb669a2d10941e07a
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 19:26:45 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,45 +8,45 @@ use App\Controllers\DashboardController;
 use App\Middleware\AuthMiddleware;
 use App\Middleware\GuestMiddleware;
 
-
-
 App::$router->get(
-'/login',
-[
-AuthController::class,
-'login'
-]
+    '/',
+    function () {
+        header("Location: /login");
+        exit;
+    }
 );
 
+App::$router->get(
+    '/login',
+    [
+        AuthController::class,
+        'login'
+    ]
+);
 
 App::$router->post(
-'/login',
-[
-AuthController::class,
-'authenticate'
-]
+    '/login',
+    [
+        AuthController::class,
+        'authenticate'
+    ]
 );
 
-
-
 App::$router->get(
-'/logout',
-[
-AuthController::class,
-'logout'
-]
+    '/logout',
+    [
+        AuthController::class,
+        'logout'
+    ]
 );
 
-
-
 App::$router->get(
-'/dashboard',
-function(){
+    '/dashboard',
+    function () {
 
-AuthMiddleware::handle();
+        AuthMiddleware::handle();
 
-(new DashboardController())
-->index();
-
-}
+        (new DashboardController())
+            ->index();
+    }
 );
