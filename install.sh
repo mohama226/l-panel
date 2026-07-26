464423cafbcc64e103978d9dd19d697241a51983
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 02:06:24 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,87 +2,64 @@
 
 set -e
 
-echo "======================================"
-echo "        l-panel Installer"
-echo "   ocserv VPN Management Panel"
-echo "======================================"
-
-INSTALL_DIR="/var/www/l-panel"
+echo "================================"
+echo " Installing l-panel"
+echo " ocserv management panel"
+echo "================================"
 
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
+    echo "Run installer as root"
     exit 1
 fi
 
 
-echo "[1/6] Detecting operating system..."
-
-
-if [ ! -f /etc/os-release ]; then
-    echo "Cannot detect operating system"
-    exit 1
-fi
+echo "[1/8] Detecting OS..."
 
 
 source /etc/os-release
 
 
-OS=$ID
+case "$ID" in
 
-
-echo "Detected OS: $OS"
-
-
-
-case $OS in
 
 ubuntu|debian)
 
-    echo "Debian based system detected"
+echo "Debian based system detected"
 
-    bash <(curl -s https://raw.githubusercontent.com/mohama226/l-panel/main/installer/install_debian.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/mohama226/l-panel/main/installer/install_debian.sh)
 
-    ;;
+;;
 
 
 almalinux|rocky|centos|rhel)
 
-    echo "RHEL based system detected"
+echo "RHEL based system detected"
 
-    bash <(curl -s https://raw.githubusercontent.com/mohama226/l-panel/main/installer/install_rhel.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/mohama226/l-panel/main/installer/install_rhel.sh)
 
-    ;;
+;;
 
 
 *)
 
-    echo ""
-    echo "Unsupported OS: $OS"
-    echo "Supported:"
-    echo "Ubuntu"
-    echo "Debian"
-    echo "AlmaLinux"
-    echo "Rocky Linux"
-    echo "CentOS"
-    exit 1
+echo "Unsupported OS: $ID"
+
+exit 1
 
 ;;
 
 esac
 
 
-
 echo ""
-echo "======================================"
-echo " l-panel installation finished"
-echo "======================================"
+echo "================================"
+echo " l-panel installed successfully"
+echo "================================"
 
 IP=$(hostname -I | awk '{print $1}')
 
-
 echo ""
-echo "Open browser:"
-echo ""
+echo "Login URL:"
 echo "http://$IP"
 echo ""
