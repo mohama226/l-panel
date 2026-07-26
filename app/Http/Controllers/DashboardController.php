3399326a0829774ff94fe21b37faeab4a7cb4321
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 00:42:04 +0330

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,12 +7,10 @@ class DashboardController extends Controller
 {
 
 
-public function index()
-{
-
-return view('dashboard.index');
-
-}
+    public function index()
+    {
+        return view('dashboard.index');
+    }
 
 
 }
