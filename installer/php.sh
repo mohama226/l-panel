bd58ceb963aed6df948ba5f7354c9bf483d250cf
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 23:17:58 +0330

    Update php.sh

--- installer/php.sh
+++ installer/php.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-
 install_php(){
 
 echo "[OK] Installing PHP 8.3"
-
-
-if [ "$OS" = "almalinux" ]; then
 
 
 dnf install -y epel-release
@@ -26,44 +22,19 @@ php-fpm \
 php-mysqlnd \
 php-mbstring \
 php-xml \
-php-bcmath \
 php-curl \
 php-zip \
 php-gd \
-php-intl \
+php-bcmath \
 php-opcache
 
 
 systemctl enable php-fpm
-
 systemctl restart php-fpm
 
 
-else
-
-
-apt update
-
-apt install -y \
-php8.3 \
-php8.3-cli \
-php8.3-fpm \
-php8.3-mysql \
-php8.3-mbstring \
-php8.3-xml \
-php8.3-bcmath \
-php8.3-curl \
-php8.3-zip
-
-
-systemctl enable php8.3-fpm
-
-systemctl restart php8.3-fpm
-
-
-fi
-
-
 php -v
+
+echo "[OK] PHP installed"
 
 }
