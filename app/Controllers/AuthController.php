319b87fc9d6cdae62dd06892cd85174d2c6de835
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 17:52:43 +0330

    Update AuthController.php

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -8,8 +8,18 @@ use App\Core\Controller;
 
 class AuthController extends Controller
 {
-    public function login():void
+    public function login(): void
     {
         $this->view('auth/login');
+    }
+
+    public function authenticate(): void
+    {
+        echo "Processing Login";
+    }
+
+    public function logout(): void
+    {
+        echo "Logout";
     }
 }
