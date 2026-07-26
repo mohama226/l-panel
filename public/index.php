c5c9c3b1810c5d425524d54361955109247caf47
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 13:15:04 +0330

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-echo "L-Panel 0.1";
+echo "<h1>L-Panel</h1>";
+echo "<h3>OCServ Management Panel</h3>";
+echo "<p>Version 0.1</p>";
