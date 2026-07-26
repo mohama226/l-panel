4da1b9c5a452fa41996930d8438cad0727d21bc3
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 01:29:27 +0330

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>L-PANEL Login</title>
-
-<style>
-
-body{
-background:#111827;
-font-family:tahoma;
-}
-
-.box{
-width:420px;
-margin:120px auto;
-background:white;
-padding:30px;
-border-radius:15px;
-}
-
-
-input{
-width:100%;
-padding:12px;
-margin:10px 0;
-}
-
-
-button{
-width:100%;
-padding:12px;
-background:#2563eb;
-color:white;
-border:0;
-border-radius:8px;
-}
-
-h2{
-text-align:center;
-}
-
-.error{
-color:red;
-text-align:center;
-}
-
-</style>
-
-</head>
-
-<body>
-
-<div class="box">
-
-<h2>L-PANEL</h2>
-
-
-@if($errors->any())
-
-<div class="error">
-{{ $errors->first() }}
-</div>
-
-@endif
-
-
 <form method="POST" action="/login">
 
 @csrf
 
 
 <input 
-type="text" 
+type="text"
 name="username"
 placeholder="Username">
 
@@ -81,15 +15,9 @@ name="password"
 placeholder="Password">
 
 
-<button>
+<button type="submit">
 Login
 </button>
 
 
 </form>
-
-</div>
-
-
-</body>
-</html>
