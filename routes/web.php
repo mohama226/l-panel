0159b0cd07fe6e6a49dadb150e4e9a015b864bea
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 00:13:39 +0330

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+
+
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+
+Route::get('/login', [
+    AuthController::class,
+    'login'
+]);
+
+
+Route::post('/login', [
+    AuthController::class,
+    'authenticate'
+]);
+
+
+Route::get('/dashboard', [
+    DashboardController::class,
+    'index'
+]);
