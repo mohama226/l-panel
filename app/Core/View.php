f2c0b4ab9a6eeb6ddddd79bca31600ef6c35f70d
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 17:51:38 +0330

    Create View.php

---
+++ app/Core/View.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+class View
+{
+    public static function render(string $view,array $data=[]): void
+    {
+        extract($data);
+
+        require ROOT_PATH."/app/Views/{$view}.php";
+    }
+}
