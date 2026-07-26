a9b359d35eb9c0eb42c51b1e78511c9a2b5577ef
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 19:06:27 +0330

    Update Migrator.php

--- app/Core/Migrator.php
+++ app/Core/Migrator.php
@@ -4,31 +4,30 @@ declare(strict_types=1);
 
 namespace App\Core;
 
+use Dotenv\Dotenv;
+
 class Migrator
 {
-
-    public function run():void
+    public function run(): void
     {
+        // Load .env
+        $dotenv = Dotenv::createImmutable('/opt/l-panel');
+        $dotenv->load();
 
+        // Initialize database
         $db = new Database();
 
-        $files =
-        glob(ROOT_PATH.'/database/migrations/*.sql');
+        echo "Migration started\n";
 
+        // Load SQL migration files
+        $files = glob(ROOT_PATH . '/database/migrations/*.sql');
 
-        foreach($files as $file){
+        foreach ($files as $file) {
 
-
-            $sql=file_get_contents($file);
-
+            $sql = file_get_contents($file);
 
             $db->connection()
                ->exec($sql);
-
-
         }
-
-
     }
-
 }
