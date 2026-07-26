a2b313946004c281788b84f273c2d31ede56ed54
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 00:48:52 +0330

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,24 +7,24 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-
-    public function show()
+    public function login()
     {
-        return view('auth.login');
+        return view('login');
     }
 
 
-    public function login(Request $request)
+    public function authenticate(Request $request)
     {
-
         $credentials = $request->validate([
-            'email'=>'required',
+            'username'=>'required',
             'password'=>'required'
         ]);
 
 
-        if(Auth::attempt($credentials))
-        {
+        if(Auth::attempt([
+            'name'=>$credentials['username'],
+            'password'=>$credentials['password']
+        ])){
 
             $request->session()->regenerate();
 
@@ -34,25 +34,18 @@ class AuthController extends Controller
 
 
         return back()->withErrors([
-            'email'=>'Login failed'
+            'username'=>'Login failed'
         ]);
 
     }
 
 
-
     public function logout(Request $request)
     {
-
         Auth::logout();
 
         $request->session()->invalidate();
 
-        $request->session()->regenerateToken();
-
-
         return redirect('/login');
-
     }
-
 }
