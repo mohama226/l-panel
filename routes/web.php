d15dbb7829be20120f7d730791571128ba976f74
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 01:37:41 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,49 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
+Route::get('/', function () {
+    return redirect('/login');
+});
 
+Route::get('/login', [
+    AuthController::class,
+    'show'
+])->name('login');
 
-Route::get('/',function(){
+Route::post('/login', [
+    AuthController::class,
+    'login'
+]);
 
-return redirect('/login');
+Route::middleware('auth:admin')->group(function () {
+
+    Route::get('/dashboard', [
+        DashboardController::class,
+        'index'
+    ]);
+
+    Route::post('/logout', [
+        AuthController::class,
+        'logout'
+    ]);
 
 });
 
+use App\Http\Controllers\GithubController;
 
+Route::middleware('auth')->group(function () {
 
-Route::get('/login',
-[
-AuthController::class,
-'show'
-])->name('login');
+    Route::get(
+        '/admin/github',
+        [GithubController::class, 'index']
+    );
 
-
-
-Route::post('/login',
-[
-AuthController::class,
-'login'
-]);
-
-
-
-
-Route::middleware('auth:admin')->group(function(){
-
-
-Route::get('/dashboard',
-[
-DashboardController::class,
-'index'
-]);
-
-
-Route::post('/logout',
-[
-AuthController::class,
-'logout'
-]);
-
+    Route::post(
+        '/admin/github/push',
+        [GithubController::class, 'push']
+    );
 
 });
