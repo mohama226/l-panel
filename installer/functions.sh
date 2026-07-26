0003d5a0dc2d874b55b8a26cac9a43eed6b7ca6b
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 22:39:41 +0330

    Create functions.sh

---
+++ installer/functions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+banner(){
+
+clear
+
+echo "
+=================================
+          L-PANEL
+       Laravel Installer
+          Version 1.0
+=================================
+"
+
+}
+
+
+success(){
+
+echo "[OK] $1"
+
+}
+
+
+error(){
+
+echo "[ERROR] $1"
+
+exit 1
+
+}
