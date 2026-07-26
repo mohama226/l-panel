790df17713cdfeef2e33b53ca69b06d26a145682
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-26 23:30:14 +0330

    Update main.sh

--- installer/main.sh
+++ installer/main.sh
@@ -1,30 +1,61 @@
 #!/bin/bash
 
+set -e
+
+
+BASE_DIR="/tmp/lpanel-installer"
+
+
+source $BASE_DIR/installer/functions.sh
+source $BASE_DIR/installer/os.sh
+source $BASE_DIR/installer/packages.sh
+source $BASE_DIR/installer/composer.sh
+source $BASE_DIR/installer/laravel.sh
+source $BASE_DIR/installer/database.sh
+source $BASE_DIR/installer/admin.sh
+source $BASE_DIR/installer/nginx.sh
+source $BASE_DIR/installer/command.sh
+
+
 banner
+
 
 detect_os
 
+
 install_packages
+
 
 install_composer
 
+
 install_dependencies
+
 
 install_laravel
 
+
 create_database
+
 
 create_env
 
+
 run_migrations
+
 
 create_admin
 
+
 configure_nginx
+
 
 create_command
 
-echo ""
-echo "=========================="
-echo " L-PANEL INSTALLED"
-echo "=========================="
+
+
+echo "
+=================================
+ L-PANEL INSTALLED SUCCESSFULLY
+=================================
+"
