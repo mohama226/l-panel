24d6294526a9d2a85be51d36b2ac6f23895aefab
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 18:53:36 +0330

    lala Update xxxx_create_ocserv_servers_table.php

--- database/migrations/xxxx_create_ocserv_servers_table.php
+++ database/migrations/xxxx_create_ocserv_servers_table.php
@@ -13,59 +13,59 @@ public function up(): void
 {
 
 
-Schema::create('ocserv_servers',function(Blueprint $table){
+Schema::create('ocserv_servers', function(Blueprint $table){
 
 
 $table->id();
 
 
+
 $table->string('name');
+
 
 
 $table->string('ip_address');
 
 
+
 $table->integer('ssh_port')
-      ->default(22);
+->default(22);
+
 
 
 $table->string('ssh_username')
-      ->default('root');
-
-
-$table->text('ssh_password')
-      ->nullable();
+->default('root');
 
 
 
 $table->integer('ocserv_port')
-      ->default(443);
+->default(443);
 
 
 
 $table->boolean('status')
-      ->default(true);
-
-
-
-$table->timestamp('last_check')
-      ->nullable();
+->default(true);
 
 
 
 $table->timestamps();
 
 
+
 });
+
 
 
 }
 
 
+
 public function down(): void
 {
 
+
 Schema::dropIfExists('ocserv_servers');
+
 
 }
 
