fae03043b035029c6b4e713164c15daf784538e7
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 19:21:20 +0330

    lala Create panel.php

---
+++ resources/lang/fa/panel.php
@@ -0,0 +1,44 @@
+<?php
+
+
+return [
+
+
+
+'name' => 'L-PANEL',
+
+
+
+'dashboard' => 'داشبورد',
+
+
+
+'admins' => 'مدیریت ادمین‌ها',
+
+
+
+'resellers' => 'نمایندگان فروش',
+
+
+
+'vpn_users' => 'کاربران VPN',
+
+
+
+'servers' => 'سرورهای OCServ',
+
+
+
+'settings' => 'تنظیمات',
+
+
+
+'logout' => 'خروج',
+
+
+
+'welcome' => 'خوش آمدید به پنل مدیریت L-PANEL',
+
+
+
+];
