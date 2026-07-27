714f6b52ad046839e70dd83b3ff7fe21f76a754b
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 17:42:11 +0330

    la Create Setting.php

---
+++ app/Models/Setting.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+
+class Setting extends Model
+{
+
+
+use HasFactory;
+
+
+
+protected $fillable=[
+
+
+'key',
+'value'
+
+
+];
+
+
+
+public static function get($key,$default=null)
+{
+
+$setting=self::where(
+    'key',
+    $key
+)->first();
+
+
+return $setting
+? $setting->value
+: $default;
+
+
+}
+
+
+
+public static function set($key,$value)
+{
+
+return self::updateOrCreate(
+
+[
+'key'=>$key
+],
+
+[
+'value'=>$value
+]
+
+);
+
+
+}
+
+
+}
