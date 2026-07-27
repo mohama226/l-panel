1e9f746e616f32a976b5b845b7fb83eb83a0d298
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 16:13:36 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,63 +3,50 @@
 set -e
 
 echo "================================"
-echo " Installing l-panel"
-echo " ocserv management panel"
+echo " l-panel Installer"
+echo " Laravel ocserv panel"
 echo "================================"
 
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Run installer as root"
+    echo "Please run as root"
     exit 1
 fi
-
-
-echo "[1/8] Detecting OS..."
 
 
 source /etc/os-release
 
 
+echo "Detected OS: $ID"
+
+
 case "$ID" in
-
-
-ubuntu|debian)
-
-echo "Debian based system detected"
-
-bash <(curl -fsSL https://raw.githubusercontent.com/mohama226/l-panel/main/installer/install_debian.sh)
-
-;;
 
 
 almalinux|rocky|centos|rhel)
 
-echo "RHEL based system detected"
+echo "RHEL based system"
 
-bash <(curl -fsSL https://raw.githubusercontent.com/mohama226/l-panel/main/installer/install_rhel.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/mohama226/l-panel/main/installer/rhel.sh)
+
+;;
+
+
+ubuntu|debian)
+
+echo "Debian based system"
+
+bash <(curl -fsSL https://raw.githubusercontent.com/mohama226/l-panel/main/installer/debian.sh)
 
 ;;
 
 
 *)
 
-echo "Unsupported OS: $ID"
+echo "Unsupported OS $ID"
 
 exit 1
 
 ;;
 
 esac
-
-
-echo ""
-echo "================================"
-echo " l-panel installed successfully"
-echo "================================"
-
-IP=$(hostname -I | awk '{print $1}')
-
-echo ""
-echo "Login URL:"
-echo "http://$IP"
-echo ""
