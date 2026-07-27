4a9007ef86fde3dfff12de9ad784a119bcb60393
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 20:15:20 +0330

    la Update Reseller.php

--- app/Models/Reseller.php
+++ app/Models/Reseller.php
@@ -4,60 +4,63 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 
 class Reseller extends Model
 {
 
 
-use HasFactory;
+    protected $fillable = [
+
+
+        'admin_id',
+
+        'user_limit',
+
+        'server_limit',
+
+        'balance',
+
+        'status'
+
+
+    ];
 
 
 
-protected $fillable=[
-
-
-'admin_id',
-'user_limit',
-'server_limit',
-'balance',
-'status'
-
-
-];
 
 
 
-protected $casts=[
 
-'balance'=>'decimal:2',
-
-'status'=>'boolean'
-
-];
+    public function admin()
+    {
 
 
+        return $this->belongsTo(
+            Admin::class
+        );
 
-public function admin()
-{
 
-return $this->belongsTo(
-    Admin::class
-);
-
-}
+    }
 
 
 
-public function vpnUsers()
-{
 
-return $this->hasMany(
-    VpnUser::class
-);
 
-}
+
+    public function vpnUsers()
+    {
+
+
+        return $this->hasMany(
+            VpnUser::class
+        );
+
+
+    }
+
+
 
 
 }
