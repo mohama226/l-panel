77e7ec44a9df42b0c95a446d709d62b7eeed2822
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 20:29:52 +0330

    dsaa Update Admin.php

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,85 +2,45 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
-
 
 class Admin extends Authenticatable
 {
-
-
     protected $table = 'admins';
 
-
-
     protected $fillable = [
-
-
         'name',
-
         'username',
-
         'password',
-
         'role',
-
         'status',
-
         'last_login'
-
-
     ];
-
-
-
-
 
     protected $hidden = [
-
-
         'password'
-
-
     ];
-
-
-
-
-
-
 
     public function reseller()
     {
-
-
         return $this->hasOne(
             Reseller::class
         );
-
-
     }
-
-
-
-
-
 
     public function vpnUsers()
     {
-
-
         return $this->hasMany(
             VpnUser::class,
             'created_by'
         );
-
-
     }
 
-
-
+    public function logs()
+    {
+        return $this->hasMany(
+            ActivityLog::class
+        );
+    }
 }
