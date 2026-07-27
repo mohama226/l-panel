dcb7df944e30beccc0fddb2d01db0c521c4cb01d
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 19:25:26 +0330

    lalaa Update VpnUserController.php

--- app/Http/Controllers/Admin/VpnUserController.php
+++ app/Http/Controllers/Admin/VpnUserController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+
+
 use App\Models\VpnUser;
+use App\Models\OcservServer;
+
+
 use App\Services\OcservService;
+
+
 use Illuminate\Http\Request;
+
+
+use Illuminate\Support\Facades\Hash;
 
 
 
@@ -14,176 +24,242 @@ class VpnUserController extends Controller
 {
 
 
-protected OcservService $ocserv;
+    protected $ocserv;
 
 
 
-public function __construct(
-    OcservService $ocserv
-)
-{
+    public function __construct(
+        OcservService $ocserv
+    )
+    {
 
-$this->ocserv=$ocserv;
+        $this->ocserv = $ocserv;
 
-}
+    }
 
 
 
 
-public function index()
-{
 
 
-$users =
-VpnUser::with([
-'server',
-'reseller'
-])
-->latest()
-->paginate(50);
 
+    public function index()
+    {
 
 
-return view(
-'vpn-users.index',
-compact('users')
-);
+        $users =
+        VpnUser::with('server')
+        ->latest()
+        ->get();
 
 
-}
 
+        return view(
+            'vpn-users.index',
+            compact('users')
+        );
 
 
+    }
 
 
-public function create()
-{
 
-return view(
-'vpn-users.create'
-);
 
-}
 
 
 
 
+    public function create()
+    {
 
-public function store(Request $request)
-{
 
+        $servers =
+        OcservServer::where(
+            'status',
+            true
+        )->get();
 
-$data=$request->validate([
 
 
-'username'=>'required|unique:vpn_users',
+        return view(
+            'vpn-users.create',
+            compact('servers')
+        );
 
-'password'=>'required',
 
-'server_id'=>'required',
+    }
 
-'expire_date'=>'nullable|date'
 
 
-]);
 
 
 
-$user=VpnUser::create($data);
 
 
+    public function store(
+        Request $request
+    )
+    {
 
-$this->ocserv
-->createUser($user);
 
 
+        $data =
+        $request->validate([
 
-return redirect()
-->route('vpn-users.index');
 
+            'username'=>
+            'required|unique:vpn_users',
 
-}
 
+            'password'=>
+            'required',
 
 
+            'server_id'=>
+            'required',
 
 
+            'expire_date'=>
+            'nullable|date'
 
-public function destroy(
-VpnUser $vpnUser
-)
-{
 
+        ]);
 
-$this->ocserv
-->deleteUser($vpnUser);
 
 
 
-$vpnUser->delete();
 
+        $data['password']
+        =
+        Hash::make(
+            $data['password']
+        );
 
 
-return back();
 
 
-}
 
+        $user =
+        VpnUser::create(
+            $data
+        );
 
 
 
 
-public function enable(
-VpnUser $vpnUser
-)
-{
 
+        $this->ocserv
+        ->createUser(
+            $user
+        );
 
-$this->ocserv
-->enableUser($vpnUser);
 
 
 
-$vpnUser->update([
 
-'status'=>true
+        return redirect()
+        ->route(
+            'vpn-users.index'
+        );
 
-]);
 
+    }
 
 
-return back();
 
 
-}
 
 
 
 
+    public function destroy(
+        VpnUser $vpnUser
+    )
+    {
 
-public function disable(
-VpnUser $vpnUser
-)
-{
 
+        $this->ocserv
+        ->deleteUser(
+            $vpnUser
+        );
 
-$this->ocserv
-->disableUser($vpnUser);
 
 
+        $vpnUser->delete();
 
-$vpnUser->update([
 
-'status'=>false
 
-]);
+        return back();
 
 
+    }
 
-return back();
 
 
-}
+
+
+
+
+
+    public function enable(
+        VpnUser $vpnUser
+    )
+    {
+
+
+        $this->ocserv
+        ->enableUser(
+            $vpnUser
+        );
+
+
+
+        $vpnUser->update([
+
+            'status'=>true
+
+        ]);
+
+
+
+        return back();
+
+
+    }
+
+
+
+
+
+
+
+
+    public function disable(
+        VpnUser $vpnUser
+    )
+    {
+
+
+        $this->ocserv
+        ->disableUser(
+            $vpnUser
+        );
+
+
+
+        $vpnUser->update([
+
+            'status'=>false
+
+        ]);
+
+
+
+        return back();
+
+
+    }
+
 
 
 }
