d9784776c5aa79823cc97c94f2b7227fc072f86a
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 20:15:47 +0330

    laa Update VpnUser.php

--- app/Models/VpnUser.php
+++ app/Models/VpnUser.php
@@ -4,131 +4,104 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 
 class VpnUser extends Model
 {
 
 
-use HasFactory;
+    protected $fillable = [
 
 
+        'username',
 
-protected $fillable=[
+        'password',
 
+        'server_id',
 
-'server_id',
-'created_by',
-'reseller_id',
-'username',
-'password',
-'expire_date',
-'traffic_limit',
-'traffic_used',
-'device_limit',
-'status',
-'last_login',
-'last_ip'
+        'reseller_id',
+
+        'created_by',
+
+        'expire_date',
+
+        'status'
 
 
-];
-
-
-
-protected $hidden=[
-
-'password'
-
-];
-
-
-
-protected $casts=[
-
-
-'expire_date'=>'date',
-
-'status'=>'boolean',
-
-'last_login'=>'datetime'
-
-
-];
+    ];
 
 
 
 
-public function server()
-{
 
-return $this->belongsTo(
-    OcservServer::class,
-    'server_id'
-);
-
-}
+    protected $casts = [
 
 
+        'expire_date'=>'date',
 
-public function creator()
-{
+        'status'=>'boolean'
 
-return $this->belongsTo(
-    Admin::class,
-    'created_by'
-);
 
-}
+    ];
 
 
 
-public function reseller()
-{
-
-return $this->belongsTo(
-    Reseller::class
-);
-
-}
 
 
 
-public function subscriptions()
-{
-
-return $this->hasMany(
-    Subscription::class
-);
-
-}
 
 
-
-/*
-|--------------------------------------------------------------------------
-| Status Helpers
-|--------------------------------------------------------------------------
-*/
+    public function server()
+    {
 
 
-public function isExpired()
-{
+        return $this->belongsTo(
+            OcservServer::class,
+            'server_id'
+        );
 
-return now()
-->greaterThan($this->expire_date);
 
-}
+    }
 
 
 
-public function remainingTraffic()
-{
 
-return $this->traffic_limit 
-       - $this->traffic_used;
 
-}
+
+
+    public function reseller()
+    {
+
+
+        return $this->belongsTo(
+            Reseller::class
+        );
+
+
+    }
+
+
+
+
+
+
+
+
+    public function creator()
+    {
+
+
+        return $this->belongsTo(
+            Admin::class,
+            'created_by'
+        );
+
+
+    }
+
+
+
 
 
 }
