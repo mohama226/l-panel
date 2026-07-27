e4fdf371f25e643fe7f02bb33fa4a3f8855483fb
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 20:22:45 +0330

    larr Create create_ocserv_servers_table.php

---
+++ database/migrations/create_ocserv_servers_table.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+
+return new class extends Migration
+{
+
+
+    public function up(): void
+    {
+
+
+
+        Schema::create('ocserv_servers', function(Blueprint $table){
+
+
+
+            $table->id();
+
+
+
+            $table->string('name');
+
+
+
+            $table->string('ip_address');
+
+
+
+            $table->string('ssh_username')
+                ->default('root');
+
+
+
+            $table->string('ssh_password')
+                ->nullable();
+
+
+
+            $table->integer('ssh_port')
+                ->default(22);
+
+
+
+            $table->boolean('status')
+                ->default(true);
+
+
+
+            $table->timestamps();
+
+
+
+        });
+
+
+
+    }
+
+
+
+
+
+    public function down(): void
+    {
+
+        Schema::dropIfExists('ocserv_servers');
+
+    }
+
+
+};
