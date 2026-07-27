f00986328b7d47d6bde68113419c3d255c889dd6
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 18:16:31 +0330

    la Create AdminAuthenticate.php

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Middleware;
+
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class AdminAuthenticate
+{
+
+
+    /**
+     * Handle an incoming request.
+     */
+
+    public function handle(
+        Request $request,
+        Closure $next
+    ): Response
+    {
+
+
+        if(!Auth::guard('admin')->check())
+        {
+
+
+            return redirect()
+                ->route('admin.login');
+
+
+        }
+
+
+
+        $admin =
+        Auth::guard('admin')->user();
+
+
+
+        if(!$admin->status)
+        {
+
+
+            Auth::guard('admin')
+                ->logout();
+
+
+
+            return redirect()
+                ->route('admin.login')
+                ->with(
+                    'error',
+                    'Account disabled'
+                );
+
+
+        }
+
+
+
+        return $next($request);
+
+
+    }
+
+
+}
