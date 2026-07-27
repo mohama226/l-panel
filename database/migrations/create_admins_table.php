465ba492ba2c3bee6624825d072a36303fb4090e
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-27 20:21:47 +0330

    lasrr Create create_admins_table.php

---
+++ database/migrations/create_admins_table.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+
+
+        Schema::create('admins', function (Blueprint $table) {
+
+
+            $table->id();
+
+
+            $table->string('name');
+
+
+            $table->string('username')
+                ->unique();
+
+
+            $table->string('password');
+
+
+            $table->enum('role',[
+
+                'superadmin',
+
+                'admin',
+
+                'reseller'
+
+            ])
+            ->default('admin');
+
+
+
+            $table->boolean('status')
+                ->default(true);
+
+
+
+            $table->timestamp('last_login')
+                ->nullable();
+
+
+
+            $table->rememberToken();
+
+
+            $table->timestamps();
+
+
+
+        });
+
+
+    }
+
+
+
+
+
+    public function down(): void
+    {
+
+        Schema::dropIfExists('admins');
+
+    }
+
+};
